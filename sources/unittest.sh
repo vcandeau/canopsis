94f98eda3f55b3052b6b845a6a0e35530d329f6c
William Pain <wpain@capensis.fr>
2011-06-08 15:15:37 +0200

    Fix output code

--- sources/unittest.sh
+++ sources/unittest.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 PREFIX="/opt/hypervision"
 
 alias python=$PREFIX/bin/python
 export PYTHONPATH=$PREFIX/lib/hyp-libs/
+
+function check_code {
+        if [ $1 -ne 0 ]; then
+                echo "Error: Code: $1"
+                exit $1
+        fi
+}
 
 
 UNITTESTS=`find ./ | grep Myunittest.py`
@@ -11,6 +18,7 @@ UNITTESTS=`find ./ | grep Myunittest.py`
 for UNITTEST in $UNITTESTS; do
 	echo "##### Proceed to $UNITTEST"
 	python $UNITTEST
+	check_code $?
 	echo "#### END ####"
 	echo
 done
