cc04100f29c896cb68fc62df31608314ab8a8fbb
William Pain <wpain@capensis.fr>
2011-06-13 20:53:53 +0200

    Complete packaging ...

---
+++ binaries/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function check_code {
+	if [ $1 -ne 0 ]; then
+		echo "Error: Code: $1"
+		exit $1
+	fi
+}
+
+function detect_os(){
+	echo "Detect distribution ..."
+	VERSION=`cat /proc/version`
+	check_code $?
+	DEBIAN=`echo "$VERSION" | grep -i debian | wc -l`
+	UBUNTU=`echo "$VERSION" | grep -i ubuntu | wc -l`
+	REDHAT=`echo "$VERSION" | grep -i redhat | wc -l`
+	CENTOS=`echo "$VERSION" | grep -i centos | wc -l`
+
+	DIST_VERS=""
+	
+	if [ $DEBIAN -ne 0 ]; then
+		DIST="DEBIAN"
+		DIST_VERS=`cat /etc/debian_version`
+		echo " + $DIST $DIST_VERS"
+	elif [ $UBUNTU -ne 0 ]; then
+		DIST="UBUNTU"
+		DIST_VERS=`lsb_release -r | cut -f2`
+		echo " + $DIST $DIST_VERS"
+	elif [ $REDHAT -ne 0 ]; then
+		DIST="REDHAT"
+		DIST_VERS=`lsb_release -r | cut -f2`
+		echo " + $DIST $DIST_VERS"
+	elif [ $CENTOS -ne 0 ]; then
+		DIST="CENTOS"
+		DIST_VERS=`lsb_release -r | cut -f2`
+		echo " + $DIST $DIST_VERS"
+	else
+		echo " + Impossible to find distribution ..."
+		exit 1
+	fi
+}
+
+detect_os
