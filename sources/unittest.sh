8273030795d33884607fb676ef7d17c925ee7353
William Pain <wpain@capensis.fr>
2011-06-15 14:01:53 +0200

    Fix permissions

--- sources/unittest.sh
+++ sources/unittest.sh
@@ -12,12 +12,15 @@ sudo su - hypervision -c 'hypcontrol start'
 
 UNITTESTS=`find ./ | grep Myunittest.py`
 
-for UNITTEST in $UNITTESTS; do
-	echo "##### Proceed to $UNITTEST"
-	sudo su - hypervision -c "python $SRC_PATH/$UNITTEST"
-	check_code $?
-	echo "#### END ####"
-	echo
-done
+for UNITTEST in $UNITTESTS; do 
+        echo "##### Proceed to $UNITTEST" 
+        sudo mkdir -p $PREFIX/tmp 
+        cp $SRC_PATH/$UNITTEST $PREFIX/tmp/ 
+        sudo su - hypervision -c "python $PREFIX/tmp/Myunittest.py" 
+        check_code $? 
+        echo "#### END ####" 
+        echo 
+done 
+sudo rm -Rf $PREFIX/tmp &> /dev/null
 
 sudo su - hypervision -c 'hypcontrol stop'
