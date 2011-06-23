11f18b40624b36c62c1fb5fccb83c1bf77cd8bac
William Pain <wpain@capensis.fr>
2011-06-23 12:06:49 +0200

    Add auto-reconnect

--- sources/canolibs/lib/canolibs/camqp.py
+++ sources/canolibs/lib/canolibs/camqp.py
@@ -44,6 +44,9 @@ class camqp(threading.Thread):
 		
 		self.queues = {};
 		
+		self.task_heartbeat = task.LoopingCall(self.heartbeat)
+		self.heartbeat_interval = 5
+		
 		try:
 			self.spec = txamqp.spec.load(os.path.expanduser("~/lib/canolibs/amqp0-8.xml"))
 		except:
@@ -57,25 +60,55 @@ class camqp(threading.Thread):
 	def on_error_mute(self, err):
 		pass
 
+	def on_connection_error(self, err):
+		self.logger.error("Connection ERROR, txAMQP dump:\n%s" % err)
+		#self.logger.error("Disconnect ...")
+		#self.disconnect()
+		self.connected = False
+		reactor.callFromThread(self.reconnect)
+
+	def on_publish_error(self, err):
+		self.logger.error("Publish ERROR, txAMQP dump:\n%s" % err)
+		self.disconnect()
+		reactor.callFromThread(self.reconnect)
+		
+	@inlineCallbacks
+	def reconnect(self):
+		if self.RUN:
+			self.logger.debug("Try to re-connect after 5 seconds ...")
+			time.sleep(5)
+			self.set_client()
+			yield self.client
+		else:
+			yield None
+		
+	def heartbeat(self):
+		self.logger.debug("Heartbeat ...")
+		if self.connected:
+			msg = Content("{'heartbeat': 1}")
+			self.publish(msg, "heartbeat")
+
+	def set_client(self):
+		self.logger.debug("Set AMQP client ...")
+		self.client = ClientCreator(reactor, AMQClient, delegate=self.delegate, vhost=self.virtual_host, spec=self.spec).connectTCP(self.host, self.port)
+		self.client.addCallback(self.on_connect)
+		self.client.addErrback(self.on_connection_error)
+
+	@inlineCallbacks
 	def run(self):
 		self.logger.debug("Start thread ...")
 		
 		self.delegate = TwistedDelegate()
 		
-		self.client = ClientCreator(reactor, AMQClient, delegate=self.delegate, vhost=self.virtual_host, spec=self.spec).connectTCP(self.host, self.port)
-		self.client.addCallback(self.on_connect)
-		self.client.addErrback(self.on_error)
-
-		self.logger.debug("Start Reactor ...")
+		self.set_client()
 		
 		if reactor.running:
-			return None
-
-		while self.RUN:
+			yield self.client
+			while self.RUN:
+				time.sleep(1)
+		else:
+			self.logger.debug("Start Reactor ...")
 			reactor.run(installSignalHandlers=0)
-			if self.RUN:
-				self.logger.debug("Try to re-connect after 5 seconds ...")
-				time.sleep(5)
 		
 	def stop(self):
 		self.logger.debug("Stop thread ...")
@@ -130,6 +163,7 @@ class camqp(threading.Thread):
 			d.addCallback(self.on_message, queue, qsettings)
 			d.addErrback(self.on_error_mute)
 		
+		self.task_heartbeat.start(self.heartbeat_interval)
 		returnValue((self.conn, self.chan))
 	
 	def add_queue(self, queue_name, routing_key, callback, exchange_name=None, no_ack = True):
@@ -176,7 +210,6 @@ class camqp(threading.Thread):
 			d = queue.get()
 			d.addCallback(self.on_message, queue, qsettings)
 			d.addErrback(self.on_error_mute)
-	
 
 	def publish(self, msg, routing_key, exchange_name=None):
 		self.wait_connection()
@@ -188,7 +221,7 @@ class camqp(threading.Thread):
 			def send_message(msg, exchange_name, routing_key):
 				#self.logger.debug("Send message to %s in %s" % (routing_key, exchange_name))
 				d = self.chan_pub.basic_publish(exchange=exchange_name, content=msg, routing_key=routing_key)
-				d.addErrback(self.on_error)
+				d.addErrback(self.on_publish_error)
 				yield d
 				
 			reactor.callFromThread(send_message, msg, exchange_name, routing_key)
@@ -196,25 +229,32 @@ class camqp(threading.Thread):
 	@inlineCallbacks
 	def disconnect(self):
  		if self.connected:
-			self.connected = False
-			
+	
 			for key in self.queues.keys():
 				qsettings = self.queues[key]
 				self.logger.debug("Close Queue '%s' ..." % qsettings['queue_name'])
 				qsettings['queue'].close()
 			
 			self.logger.debug("Close channel 2 ...")
-			yield self.chan_pub.channel_close()
+			d = self.chan_pub.channel_close()
+			d.addErrback(self.on_error_mute)
+			yield d
 			
 			self.logger.debug("Close channel 1 ...")
-			yield self.chan.channel_close()
+			d = self.chan.channel_close()
+			d.addErrback(self.on_error_mute)
+			yield d
 			
 			self.logger.debug("Close Connection ...")
 			chan0 = yield self.conn.channel(0)
-			yield chan0.connection_close()
+			d = chan0.connection_close()
+			d.addErrback(self.on_error_mute)
+			yield d
 			self.logger.debug("Disconnected.")
 		
-		if reactor.running:
+		self.task_heartbeat.stop()
+		self.connected = False
+		if reactor.running and not self.RUN:
 			self.logger.debug("Stop Reactor ...")
 			reactor.stop()
 
