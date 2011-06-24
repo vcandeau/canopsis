29c873d10a439e90aefd086c71443e59a37e9c4a
William Pain <wpain@capensis.fr>
2011-06-24 11:06:43 +0200

    First structure of webcore

---
+++ sources/webcore/opt/webcore/webserver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys, os
+
+BASE_PATH=os.path.expanduser("~/var/www/")
+PORT=8080
+sys.path.append(os.path.expanduser("~/opt/webcore/services/"))
+
+import tornado.ioloop
+import tornado.web
+
+import auth
+
+settings = {
+	"debug": True,
+    "static_path": BASE_PATH+"/html/",
+    "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+    "login_url": "/static/index.html",
+    "xsrf_cookies": True,
+}
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.redirect("/static/index.html")
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    
+#    (r"/webcore/auth/([a-zA-Z]*)", auth.authHandler),
+    
+], **settings)
+
+
+if __name__ == "__main__":
+	application.listen(PORT)
+	try:
+		tornado.ioloop.IOLoop.instance().start()
+	except:
+		pass
