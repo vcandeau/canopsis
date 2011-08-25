1a63cb1e597103ba72150b0496e6e14bf80f0596
William Pain <wpain@capensis.fr>
2011-08-25 15:43:59 +0200

    First release of selector's API

---
+++ sources/canolibs/lib/canolibs/cselector.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+#import logging
+from crecord import crecord
+from ccache import ccache
+from ctimer import ctimer
+
+import json
+
+class cselector(crecord):
+	def __init__(self, name, storage=None, *args):
+
+		crecord.__init__(self, *args)
+
+		#if isinstance(record, crecord):
+		#	crecord.__init__(self, raw_record=record.dump())
+		#else:
+		#	crecord.__init__(self, *args)
+
+		self.type = "selector"
+		
+		if storage:
+			self.storage = storage
+			self.cache = ccache(storage)
+		else:
+			raise Exception('You must specify storage !')
+
+		self.timer = ctimer()
+
+		self.data['mfilter'] = {}
+		self.data['last_resolv_time'] = 0
+		self.data['last_nb_records'] = 0
+
+		self._id = "selector-"+self.storage.default_account.user+"-"+name
+		
+		try:
+			record = self.storage.get(self._id)
+			self.load(record.dump())
+		except:
+			pass
+
+	def set_filter(self, mfilter):
+		self.data['mfilter']=json.dumps(mfilter)
+
+	def save(self):
+		self.storage.put(self)
+
+	def resolv(self):
+		self.timer.start()	
+		## get from cache	
+		#_ids = self.cache.get(self._id, 10)
+		#if _ids or _ids == []:
+		#	return _ids
+
+		# resolv filter ...
+		_ids = []
+		mfilter = json.loads(self.data['mfilter'])
+		records = self.storage.find(mfilter)	
+		#for record in records:
+		#	_ids.append(record._id)
+	
+		# Put in cache
+		#self.cache.put(self._id, _ids)
+		self.timer.stop()
+		self.data['last_resolv_time'] = self.timer.elapsed
+		self.data['last_nb_records'] = len(records)
+
+		self.save()
+
+		return records
+
+	def cat(self):
+		print "Id:\t\t\t", self._id
+		print "Mfilter:\t\t", self.data['mfilter']
+		print "Last Resolv Time:\t%.2f ms" % (self.data['last_resolv_time']*1000)
+		print "Last Nb records:\t", self.data['last_nb_records'], "\n"
+
+
+#################
+
+#def cselector_getall(storage):
+#	selectors = []
+#	records = storage.find({'crecord_type': 'selector'})
+#	for record in records:
+#		selectors.append(cselector(record))
+#	
+#	return selectors
+
+#def cselector_get(storage, user):
+#	record = storage.get('account-'+user)
+#	selector = cselector(record)
+#	return selector
+	
