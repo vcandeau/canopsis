fe778eacce25bcb7331a6079a2f4cb28eb3b7e2f
William Pain <wpain@capensis.fr>
2011-08-26 14:38:35 +0200

    Add match function

--- sources/canolibs/lib/canolibs/unittest/cselector-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/cselector-Myunittest.py
@@ -13,7 +13,7 @@ import hashlib
 
 STORAGE = None
 SELECTOR = None
-
+ID = None
 class KnownValues(unittest.TestCase): 
 	def setUp(self):
 		pass
@@ -23,11 +23,13 @@ class KnownValues(unittest.TestCase):
 		SELECTOR = cselector(name="myselector", storage=STORAGE)
 
 	def test_02_PutData(self):
-		record1 = crecord({'check': 'test1'})
+		global ID
+		record1 = crecord({'_id': 'check1', 'check': 'test1'})
 		record2 = crecord({'check': 'test2'})
 		record3 = crecord({'check': 'test3'})
 
 		STORAGE.put([record1, record2, record3])
+		ID = record2._id
 
 	def test_03_Resolv(self):
 		SELECTOR.set_filter({'$or': [ {'check': 'test1'},  {'check': 'test2'}] })
@@ -38,11 +40,22 @@ class KnownValues(unittest.TestCase):
 	def test_04_Cat(self):
 		SELECTOR.cat()
 
-	def test_05_Remove(self):
+	def test_05_Match(self):
+		if not SELECTOR.match('check1'):
+			raise Exception('Error in match, plain id ...')
+
+		if SELECTOR.match('toto'):
+			raise Exception('Error in match, wrong id ...')
+
+		if not SELECTOR.match(ID):
+			raise Exception('Error in match, objectid ...')
+
+	def test_06_Remove(self):
 		STORAGE.remove(SELECTOR)
 
 	def test_99_DropNamespace(self):
 		STORAGE.drop_namespace('unittest')
+
 
 if __name__ == "__main__":
 	STORAGE = cstorage(caccount(user="root", group="root"), namespace='unittest')
