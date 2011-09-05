eda7c5e647857e228e17161fe2754fb995a6d013
William Pain <wpain@capensis.fr>
2011-09-05 15:13:38 +0200

    New webcore

--- sources/webcore/opt/webcore/webserver.py
+++ sources/webcore/opt/webcore/webserver.py
@@ -2,9 +2,9 @@
 
 import sys, os, logging
 import ConfigParser
-import tornado.ioloop
-import tornado.web
-import tornado.options
+
+import bottle
+from bottle import route, run, static_file, redirect
 
 ## Read config file
 config = ConfigParser.RawConfigParser()
@@ -14,8 +14,11 @@ config.read(os.path.expanduser('~/etc/webcore.conf'))
 root_directory=os.path.expanduser(config.get("server", "root_directory"))
 port=config.getint("server", "port")
 debug=config.getboolean("server", "debug")
+debug = True
 
-ws_dir=os.path.expanduser("~/opt/webcore/webservices/")
+bottle.debug(debug)
+
+ws_dir=os.path.expanduser("~/opt/webcore/libexec/")
 sys.path.append(ws_dir)
 
 ## Logger
@@ -26,31 +29,19 @@ else:
 logging.basicConfig(level=logging_level,
 		format='%(asctime)s %(name)s %(levelname)s %(message)s',
 )
-logger = logging.getLogger("tornado")
+logger = logging.getLogger("webcore")
 
-## Tornado settings
-settings = {
-	"debug": debug,
-	"static_path": root_directory,
-	"cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-	"login_url": "/static/canopsis/index.html",
-	#"xsrf_cookies": True,
-}
+## Basic Handler
+@route('/static/:path#.+#')
+def server_static(path):
+	return static_file(path, root=root_directory)
 
-## Handler
-class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.redirect("/static/canopsis/index.html")
+@route('/')
+@route('/index.html')
+def index():
+	redirect("/static/canopsis/index.html")
 
-class WebservicesHandler(tornado.web.RequestHandler):
-	def get(self, ws, action):
-		self.redirect("/static/canopsis/index.html")
-
-
-handlers = [ (r"/", MainHandler) ]
-
-
-## Load webservices
+## Load libexec
 for ws in os.listdir(ws_dir):
 	if ws[0] != "." :
 		ext = ws.split(".")[1]
@@ -59,31 +50,17 @@ for ws in os.listdir(ws_dir):
 			logger.debug("Trying to load webservice '%s'" % ws)
 			try:
 				exec "import %s" % ws
-				exec "handlers.append((r'/webservices/%s/([a-zA-Z0-9_/-]*)', %s.Handler))" % (ws, ws)
 				logger.debug("\tLoaded successfully ...")
 			except Exception, err:
 				logger.error("\t%s" % err)
 				logger.error("\tImpossible to load.")
 
-#print handlers
-#class TestJSONHandler():
-#	def __init__():
-#		pass
-
-#body_handlers = [
-#    ("application/json", TestJSONHandler),
-#]
-
 def main():
-	application = tornado.web.Application(handlers, **settings)
-	tornado.options.enable_pretty_logging() 
-	#application.listen(port, body_handlers=body_handlers)
-	application.listen(port)
-	try:
-		logger.debug("Start listenning on port %i" % port)
-		tornado.ioloop.IOLoop.instance().start()
-	except:
-		pass
+		try:
+			logger.debug("Start listenning on port %i" % port)
+			run(host='localhost', port=port, reloader=debug)
+		except:
+			pass
 
 if __name__ == "__main__":
 	main()
