6bc0800b1fc9d5374e99248f1bf2d96b85e18695
William Pain <wpain@capensis.fr>
2011-09-29 14:11:32 +0200

    Perfdata parsing

--- sources/canolibs/lib/canolibs/unittest/ctools-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/ctools-Myunittest.py
@@ -10,23 +10,26 @@ class KnownValues(unittest.TestCase):
 		pass
 
 	def test_01_Perfdata(self):
-		result = {'load1': {'warn': '5.000', 'crit': '10.000', 'metric': 'load1', 'value': '0.440', 'min': '0'}, 'load15': {'warn': '3.000', 'crit': '4.000', 'metric': 'load15', 'value': '0.130', 'min': '0'}, 'load5': {'warn': '4.000', 'crit': '6.000', 'metric': 'load5', 'value': '0.190', 'min': '0'}}
+		result = {'load1': {'warn': 5.0, 'crit': 10.0, 'metric': 'load1', 'value': 0.44, 'min': 0.0}, 'load15': {'warn': 3.0, 'crit': 4.0, 'metric': 'load15', 'value': 0.13, 'min': 0.0}, 'load5': {'warn': 4.0, 'crit': 6.0, 'metric': 'load5', 'value': 0.19, 'min': 0.0}}
 		perf_data = "load1=0.440;5.000;10.000;0; load5=0.190;4.000;6.000;0; load15=0.130;3.000;4.000;0;"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
+			print perf_data
 			raise Exception('Error in perfdata parsing ...')
 
-		result = {'load1': {'metric': 'load1', 'value': '0.440'}}
+		result = {'load1': {'metric': 'load1', 'value': 0.440}}
 		perf_data = "load1=0.440"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
+			print perf_data
 			raise Exception('Error in perfdata parsing ...')
 
 
-		result = {'warn': {'min': '0', 'max': '100', 'metric': 'warn', 'value': '0', 'warn': '0', 'crit': '0', 'unit': '%'}, 'crit': {'min': '0', 'max': '100', 'metric': 'crit', 'value': '0', 'warn': '0', 'crit': '0', 'unit': '%'}, 'ok': {'min': '0', 'max': '100', 'metric': 'ok', 'value': '100.0', 'warn': '98', 'crit': '95', 'unit': '%'}}
+		result = {'warn': {'min': 0.0, 'max': 100.0, 'metric': 'warn', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, 'crit': {'min': 0.0, 'max': 100.0, 'metric': 'crit', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, 'ok': {'min': 0.0, 'max': 100.0, 'metric': 'ok', 'value': 100.0, 'warn': 98.0, 'crit': 95.0, 'unit': '%'}}
 		perf_data = "'ok'=100.0%;98;95;0;100 'warn'=0%;0;0;0;100 'crit'=0%;0;0;0;100"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
+			print perf_data
 			raise Exception('Error in perfdata parsing ...')
 
 	def test_02_calcul_pct(self):
