df5c303b0334d3733f67051cce6ee873c8815277
William Pain <wpain@capensis.fr>
2011-10-10 15:15:43 +0200

    Add makeevent

--- sources/canolibs/lib/canolibs/ctools.py
+++ sources/canolibs/lib/canolibs/ctools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import re, logging
+import re, logging, socket, time
 
 legend = ['ok','warning','critical','unknown']
 
@@ -106,4 +106,21 @@ def dynmodloads(path=".", subdef=False, pattern=".*"):
 		pass
 
 	return loaded
+
+def make_event(service_description, source_name='internal', source_type='service', host_name=None, state_type=1, state=0, output='', perf_data=''):
+	if not host_name:
+		host_name = socket.gethostname()
+
+	dump = {}
+	dump['source_name'] = source_name
+	dump['source_type'] = source_type
+	dump['service_description'] =  service_description
+	dump['host_name'] = host_name
+	dump['rk'] = 'eventsource.canopsis.' + dump['source_name'] + '.check.'+ dump['source_type'] + "." + dump['host_name'] + "." + dump['service_description']
+	dump['state_type'] = state_type
+	dump['state'] = state
+	dump['output'] = output
+	dump['timestamp'] = int(time.time())
+	dump['perf_data'] = perf_data
+	return dump
 
