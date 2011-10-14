b3a6d2b48d6183217de1bc9154283f6498952a4d
William Pain <wpain@capensis.fr>
2011-10-14 16:18:35 +0200

    Set history and opacity

---
+++ sources/webcore/var/www/html/canopsis/app/controller/Notify.js
@@ -0,0 +1,46 @@
+Ext.define('canopsis.controller.Notify', {
+    extend: 'Ext.app.Controller',
+
+    opacity: 0.8,
+    history: false,
+    views: [],
+
+    init: function() {
+	this.getController('WebSocket').on('message', function(ws, evt) {
+		var data = Ext.JSON.decode(evt.data)
+		
+		var type = undefined;
+		var icon = undefined;
+		var title = data['source_name'] + " > " + data['host_name'];
+		var message = data['output'];
+
+		try{
+			title = title + " > " + data['service_description']
+		} catch(err) {
+		
+		}
+
+		if (data['state'] == 0){
+			icon = 'ui-icon ui-icon-check'
+		}else if (data['state'] == 1) {
+			icon = 'ui-icon ui-icon-alert'
+		}else if (data['state'] == 2){
+			type = 'error'
+		}
+
+		this.notify(title, message, type, icon);
+	}, this);
+
+    },
+
+    notify: function(title, text, type, icon){
+	$.pnotify({
+		pnotify_title: title,
+		pnotify_text: text,
+		pnotify_type: type,
+		pnotify_history: this.history,
+		pnotify_notice_icon: icon,
+		pnotify_opacity: this.opacity,
+	});
+   }
+});
