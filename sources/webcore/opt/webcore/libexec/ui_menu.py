c1d5d97bab341bbc87c28502e133ddae8723271c
Tristan Carranante <tcarranante@capensis.fr>
2011-10-21 11:02:49 +0200

    auto listing views in menu

--- sources/webcore/opt/webcore/libexec/ui_menu.py
+++ sources/webcore/opt/webcore/libexec/ui_menu.py
@@ -26,11 +26,34 @@ logger = logging.getLogger("ui-menu")
 def get_all_menu():
 	_id = request.params.get('node', default=[])
 
+	#if _id == 'root' means we want all menu
 	if _id == 'root':
 		_id = []
 
 	account = get_account()
 	storage = get_storage(namespace='object')
+	
+	#if _id == 'menu.view' catch all view, parsed them and give them in json
+	if _id == 'menu.view':
+		logger.debug('menu.view catched ------------------------------------------------->')
+		mfilter = {'crecord_type': 'view'}
+		
+		records = storage.find(mfilter,account=account)
+		#logger.debug(str(records))
+		output = []
+		for record in records:
+			logger.debug(str(record.dump()))
+			data = record.data
+			#data['id'] = str(record._id)
+			data['text'] = record.name
+			data['view'] = record._id
+			data['leaf'] = True
+			output.append(data)
+				
+		output = json.dumps(output)
+		#logger.debug(" + Output: "+str(output))
+		return output
+	###############
 
 	logger.debug("Get menu:")
 	logger.debug(" + Node: "+str(_id))
