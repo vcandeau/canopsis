c1309952dd0483fd4a02db053e4d06a2a4c57bb0
Tristan Carranante <tcarranante@capensis.fr>
2011-10-21 12:32:19 +0200

    add model ViewEditor

---
+++ sources/webcore/var/www/canopsis/app/model/ViewEditor.js
@@ -0,0 +1,10 @@
+Ext.define('canopsis.model.ViewEditor', {
+    extend: 'Ext.data.Model',
+    fields: [
+		{name: 'id'},
+		{name: 'crecord_name'},
+		{name: 'column'},
+		{name: 'lines'},
+		{name: 'hunit'},
+		],
+});
