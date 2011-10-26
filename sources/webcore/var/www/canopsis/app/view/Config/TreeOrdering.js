8d3a0463f8125944be8093f538040358a4367325
Tristan Carranante <tcarranante@capensis.fr>
2011-10-26 14:33:16 +0200

    follow naming convention

---
+++ sources/webcore/var/www/canopsis/app/view/Config/TreeOrdering.js
@@ -0,0 +1,77 @@
+Ext.define('canopsis.view.Config.TreeOrdering' ,{
+	extend: 'Ext.tree.Panel',
+	alias : 'widget.TreeOrdering',
+	model : 'widget',
+	
+	//store: store,
+	id: 'TreeOrdering',
+	
+	viewConfig: {
+		plugins: {
+			ptype: 'treeviewdragdrop'
+		}
+	},
+	//height: 300,
+	//width: 250,
+	title: 'User config panel',
+	//useArrows: true,
+	rootVisible: false,	
+	//toggleOnDblClick: false,
+	/*selType: 'cellmodel',
+	plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+            clicksToEdit: 2
+    })],*/
+    
+    bbar: [{
+		text : 'delete selected row',
+		action : 'deleteRow'
+	},{
+		xtype: 'tbseparator'
+	},{
+		text : 'clear all',
+		action : 'reset'
+	}],
+
+	columns: [{
+		xtype: 'treecolumn', //this is so we know which column will show the tree
+		text: 'Option name',
+		flex: 1,
+		sortable: true,
+		dataIndex: 'xtype'
+	},{
+		//we must use the templateheader component so we can use a custom tpl
+		//xtype: 'templatecolumn',
+		text: 'title',
+		flex: 1,
+		sortable: true,
+		dataIndex: 'title',
+		//align: 'center',
+	},{
+		text: 'colspan',
+		flex: 1,
+		dataIndex: 'colspan',
+	},{
+		text: '_id',
+		flex: 1,
+		dataIndex: '_id',
+	},{
+		text: 'refresh interval',
+		flex: 1,
+		dataIndex: 'rinterval',
+	}],
+	
+	/*root: {
+        name: 'Root',
+        expanded: true,
+	},*/
+	
+	initComponent: function() {
+
+		this.callParent(arguments);
+		
+		
+	}
+	
+	
+});
