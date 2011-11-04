764964f005bd0a44f4a81f232470defd087719e7
William Pain <wpain@capensis.fr>
2011-11-04 16:59:00 +0100

    Re-factoring ...

--- sources/webcore/var/www/canopsis/app/view/ViewEditor/View.js
+++ sources/webcore/var/www/canopsis/app/view/ViewEditor/View.js
@@ -1,65 +1,22 @@
 Ext.define('canopsis.view.ViewEditor.View' ,{
-	extend: 'Ext.grid.Panel',
+	extend: 'canopsis.lib.view.cgrid',
+
 	alias : 'widget.ViewEditor',
 	
-	
-	
-	//model: 'view',
-	store: 'ViewEditor',
-	rootVisible: false,	
-	id : 'ViewEditor',
+	model: 'view',
+	store: 'View',
 
-    title: 'Available Views',
-    /*
-    viewConfig: {
-		plugins: {
-			ptype: 'treeviewdragdrop'
-		}
-	},
-    */
-    
-    multiSelect: true,
-    
-	tbar: [ {
-				iconCls: 'icon-add',
-				text: 'Add new view',
-				itemId: 'addButton',
-			},{
-				iconCls: 'icon-delete',
-				text: 'Delete',
-				itemId: 'deleteButton',
-				disabled: true,
-			}],
-
+   
 	columns: [{
-					header: 'name',
-					flex: 2,
-					sortable: true,
-					dataIndex: 'name',
-				},{
-					header: 'id',
-					flex: 2,
-					sortable: true,
-					dataIndex: 'id',
-				}/*,
-				{
-					header: 'items',
-					flex: 2,
-					sortable: true,
-					dataIndex: 'items',
-				}*/
-	],
-
-	initComponent: function() {
-			this.callParent(arguments);
-		},
-		
-	beforeDestroy : function() {
-		log.debug("Destroy items ...")
-		canopsis.view.Tabs.Content.superclass.beforeDestroy.call(this);
-		log.debug(this.id + " Destroyed.")
-	}
-
-	
+			header: 'name',
+			flex: 2,
+			sortable: true,
+			dataIndex: 'crecord_name',
+		},{
+			header: 'id',
+			flex: 2,
+			sortable: true,
+			dataIndex: 'id',
+		}],	
 			
 });
