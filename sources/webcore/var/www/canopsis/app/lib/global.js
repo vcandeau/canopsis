5fb2be5ae1be341c43be0bc0edbba1903cf65383
William Pain <wpain@capensis.fr>
2011-11-07 14:52:40 +0100

    Oups

---
+++ sources/webcore/var/www/canopsis/app/lib/global.js
@@ -0,0 +1,15 @@
+var global = {
+	colors: {
+		up: '#50b432',
+		down: '#ed241b',
+		unreachable: '#f0f0ff',
+		ok : '#50b432',
+		warning: '#ed941b',
+		critical: '#ed241b',
+		unknown: '#f0f0ff' 
+	},
+	log: {
+		level: 5,
+		buffer: 50
+	}
+}
