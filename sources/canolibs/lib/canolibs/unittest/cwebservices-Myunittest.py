912d6d53cee0fb6b39ba4cb5c558b4d42cb172b4
William Pain <wpain@capensis.fr>
2011-11-07 10:38:46 +0100

    Unittest + clean

--- sources/canolibs/lib/canolibs/unittest/cwebservices-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/cwebservices-Myunittest.py
@@ -6,6 +6,22 @@ from cwebservices import cwebservices
 
 WS = None
 
+get_auth_uri = [
+	# account
+	'/account/me',
+	'/account/',
+	'/account/account.root',
+	# rest
+	'/rest/object',
+	'/rest/object/account',
+	'/rest/object/account/account.root',
+	# ui_menu
+	#'/ui/menu',
+	# ui_view
+	'/ui/dashboard',
+	#'/ui/views', !!!!!!!!!!!
+]
+
 class KnownValues(unittest.TestCase): 
 	def setUp(self):
 		pass
@@ -14,11 +30,26 @@ class KnownValues(unittest.TestCase):
 		global WS
 		WS = cwebservices()
 
-	def test_02_Login(self):
+	def test_02_TestURL(self):
+		for uri in get_auth_uri:
+			success=False
+			try:
+				WS.get(uri)
+				success=True
+			except:
+				pass
+		
+			if success:
+				raise Exception("%s is open !" % uri)
+
+	def test_03_Login(self):
 		WS.login('root', 'root')
 
-	def test_03_Rest(self):
-		print WS.get("/rest/object/account")
+	def test_04_TestURLAuthed(self):
+		WS.login('root', 'root')
+
+		for uri in get_auth_uri:
+			WS.get(uri)
 
 	def test_99_Logout(self):
 		WS.logout()
