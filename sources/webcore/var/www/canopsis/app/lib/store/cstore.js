cb08382e146cc608664e0c7ec2b77ec1c9a49def
Tristan Carranante <tcarranante@capensis.fr>
2011-11-09 16:10:12 +0100

    forget them

---
+++ sources/webcore/var/www/canopsis/app/lib/store/cstore.js
@@ -0,0 +1,16 @@
+Ext.define('canopsis.lib.store.cstore', {
+    extend: 'Ext.data.Store',
+    
+    //raise an exception if server didn't accept the request
+	//and display a popup if the store is modified
+    listeners: {
+		exception: function(proxy, response, operation){
+			Ext.MessageBox.show({
+				title: 'REMOTE EXCEPTION',
+				msg: this.storeId + ': request failed',
+				icon: Ext.MessageBox.ERROR,
+				buttons: Ext.Msg.OK
+			});
+		},
+   }	
+});
