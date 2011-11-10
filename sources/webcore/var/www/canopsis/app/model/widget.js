c415b35c4c81fb98cd3a4cf527b8b7b886e043df
William Pain <wpain@capensis.fr>
2011-11-10 14:34:41 +0100

    New model for widget

--- sources/webcore/var/www/canopsis/app/model/widget.js
+++ sources/webcore/var/www/canopsis/app/model/widget.js
@@ -1,12 +1,19 @@
 Ext.define('canopsis.model.widget', {
     extend: 'Ext.data.Model',
     fields: [
+		{name : 'name'},
+		{name : 'description',		defaultValue: undefined},
+		{name : 'version',		defaultValue: undefined},
+		{name : 'author',		defaultValue: undefined},
+		{name : 'website',		defaultValue: undefined},
+		{name : 'options',		defaultValue: undefined},
 		{name : 'xtype'},
 		{name : 'colspan',		defaultValue: 1},
 		{name : 'rowspan',		defaultValue: 1},
 		{name : 'refreshInterval', 	defaultValue: undefined},
 		{name : 'nodeId',		defaultValue: undefined},
 		{name : 'title', 		defaultValue: undefined},
+		{name : 'border', 		defaultValue: false},
 		{name : 'rowHeight', defaultValue: undefined}
 		]
 });
