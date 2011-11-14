ce819266c662aeaa1021c2fd8ce563aef3e8f393
Tristan Carranante <tcarranante@capensis.fr>
2011-11-14 11:23:08 +0100

    add auto loading widgets

--- sources/webcore/var/www/canopsis/app/controller/Widgets.js
+++ sources/webcore/var/www/canopsis/app/controller/Widgets.js
@@ -1,9 +1,22 @@
 Ext.define('canopsis.controller.Widgets', {
     extend: 'Ext.app.Controller',
 
-    views: ['Widgets.kpi', 'Widgets.line_graph', 'Widgets.host_header'],
-    //stores: ['View']
+    //views: ['Widgets.kpi', 'Widgets.host_header'],
+    stores: ['Widget'],
 
     init: function() {
+		Ext.Loader.setPath('widgets', './widgets');
+		var store = this.getStore('Widget');
+		log.debug('[controller][Widgets] : parsing Widget store');
+		store.on('load',function(){
+			this.each(function(record){
+				log.debug('[controller][Widgets] : loading' + 'record.data.xtype');
+				var name ="widgets." + record.data.xtype + "." + record.data.xtype ;
+				Ext.require(name);
+			});
+			
+		});
     },
+    
+	
 });
