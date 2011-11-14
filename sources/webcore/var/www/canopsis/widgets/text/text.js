0f49feb938219bc9b5ab1765d21e69ac43e0c7d6
William Pain <wpain@capensis.fr>
2011-11-14 13:09:01 +0100

    Set text widget

--- sources/webcore/var/www/canopsis/widgets/text/text.js
+++ sources/webcore/var/www/canopsis/widgets/text/text.js
@@ -2,5 +2,16 @@ Ext.define('widgets.text.text' ,{
 	extend: 'canopsis.lib.view.cwidget',
 	
 	alias : 'widget.text',
+
+	initComponent: function() {
+		this.nodeId = false
+		this.callParent(arguments);
+	},
+
+	doRefresh: function (){
+		if (this.text){
+			this.setHtml(this.text);
+		}
+	}
 	
 });
