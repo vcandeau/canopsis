c7adf06ce64fcb9182767c97f9a82fc6b2492c5d
William Pain <wpain@capensis.fr>
2011-11-15 18:30:19 +0100

    Improve TPL

--- sources/webcore/var/www/canopsis/widgets/host_header/host_header.js
+++ sources/webcore/var/www/canopsis/widgets/host_header/host_header.js
@@ -2,11 +2,14 @@ Ext.define('widgets.host_header.host_header' ,{
 	extend: 'canopsis.lib.view.cwidget',
 
 	alias : 'widget.host_header',
+	
+	tpl: '<h1>{source_name} > {type} > {source_type} > {host_name}</h1>',
 
-	htmlTpl: new Ext.Template('<strong>{id}</strong>', {compiled: true}),
+	htmlTpl: false,
 
 	initComponent: function() {
 		this.refreshInterval = 0
+		this.htmlTpl = new Ext.Template(this.tpl, {compiled: true})
 		this.callParent(arguments);		
 	},
 
