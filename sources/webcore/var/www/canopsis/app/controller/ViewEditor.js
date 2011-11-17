b290e586c92ffb2d333d96f6e6d84dcb87d345b1
Tristan Carranante <tcarranante@capensis.fr>
2011-11-17 18:08:09 +0100

    no empty view name

--- sources/webcore/var/www/canopsis/app/controller/ViewEditor.js
+++ sources/webcore/var/www/canopsis/app/controller/ViewEditor.js
@@ -19,59 +19,61 @@ Ext.define('canopsis.controller.ViewEditor', {
 	},
 	
 	_saveForm : function(form){
+		if (form.GlobalOptions.getForm().isValid()){
 		
-		var store = this.grid.store;
-		var record = Ext.create('canopsis.model.view', data);
-		
-		log.debug('[controller][cgrid][form] - Saving form');
-		
-		//parsing items
-		var temptab = [];
-		
-		form.ItemsStore.each(function(record) {
-			temptab.push(record.data);
-		});
-		record.set('items', temptab);
-		
-		//crecord_name and name fixing
-		var record_name = form.down('textfield[name=crecord_name]').getValue()
-		record.set('crecord_name',record_name);
-		//log.debug('crecord_name : ' + record.get('crecord_name'));
-		if (form.recordName){
-			record.set('id',form.recordName);
-		} else {
-			record.set('id','view.'+ global.account.user + '.' + record_name);
+			var store = this.grid.store;
+			var record = Ext.create('canopsis.model.view', data);
+			
+			log.debug('[controller][cgrid][form] - Saving form');
+			
+			//parsing items
+			var temptab = [];
+			
+			form.ItemsStore.each(function(record) {
+				temptab.push(record.data);
+			});
+			record.set('items', temptab);
+			
+			//crecord_name and name fixing
+			var record_name = form.down('textfield[name=crecord_name]').getValue()
+			record.set('crecord_name',record_name);
+			//log.debug('crecord_name : ' + record.get('crecord_name'));
+			if (form.recordName){
+				record.set('id',form.recordName);
+			} else {
+				record.set('id','view.'+ global.account.user + '.' + record_name);
+			}
+			//log.debug('id : ' + record.get('id'));
+			record.set('refreshInterval',form.down('numberfield[name=refreshInterval]').getValue());
+			//log.debug('refreshInterval : ' + record.get('refreshInterval'));
+			record.set('nbColumns',form.down('numberfield[name=nbColumns]').getValue());
+			//log.debug('nbColumns : ' + record.get('nbColumns'));
+			record.set('rowHeight',form.down('numberfield[name=rowHeight]').getValue());
+			//log.debug('rowHeight : ' + record.get('rowHeight'));
+			
+			
+			//get nodeId if defined
+			panel = form.GlobalOptions.down('gridpanel');
+			_nodeId = panel.store.getAt(0);
+			//log.debug(_nodeId);
+			if (_nodeId){
+				log.debug('there is a nodeId :');
+				log.debug(_nodeId.get('id'));
+				record.set('nodeId', _nodeId.get('id'));
+			}
+			
+			log.debug('[controller][cgrid][form] - Store record in store');
+			output = store.add(record);
+			log.debug('this record have added to store(what add() have returned) :')
+			log.debug(output);
+			store.load();
+			
+			//reload menu view
+			Ext.data.StoreManager.lookup('Menu').load();
+			
+			
+			this._cancelForm(form);
 		}
-		//log.debug('id : ' + record.get('id'));
-		record.set('refreshInterval',form.down('numberfield[name=refreshInterval]').getValue());
-		//log.debug('refreshInterval : ' + record.get('refreshInterval'));
-		record.set('nbColumns',form.down('numberfield[name=nbColumns]').getValue());
-		//log.debug('nbColumns : ' + record.get('nbColumns'));
-		record.set('rowHeight',form.down('numberfield[name=rowHeight]').getValue());
-		//log.debug('rowHeight : ' + record.get('rowHeight'));
-		
-		
-		//get nodeId if defined
-		panel = form.GlobalOptions.down('gridpanel');
-		_nodeId = panel.store.getAt(0);
-		//log.debug(_nodeId);
-		if (_nodeId){
-			log.debug('there is a nodeId :');
-			log.debug(_nodeId.get('id'));
-			record.set('nodeId', _nodeId.get('id'));
-		}
-		
-		log.debug('[controller][cgrid][form] - Store record in store');
-		output = store.add(record);
-		log.debug('this record have added to store(what add() have returned) :')
-		log.debug(output);
-		store.load();
-		
-		//reload menu view
-		Ext.data.StoreManager.lookup('Menu').load();
-		
-		
-		this._cancelForm(form);
 	},
 
 	beforeload_EditForm: function(form){
