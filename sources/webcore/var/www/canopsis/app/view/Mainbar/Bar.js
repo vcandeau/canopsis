bde211413fbe57932e81a85b5996d0b604e376b1
William Pain <wpain@capensis.fr>
2011-11-21 16:09:58 +0100

    Add top toolbar

---
+++ sources/webcore/var/www/canopsis/app/view/Mainbar/Bar.js
@@ -0,0 +1,56 @@
+Ext.define('canopsis.view.Mainbar.Bar' ,{
+	extend: 'Ext.toolbar.Toolbar',
+
+	alias: 'widget.Mainbar',
+
+	border: false,
+
+	layout: {
+		type: 'hbox',
+		align: 'stretch',
+		//padding: 5
+	},
+
+	baseCls: 'Mainbar',
+
+	initComponent: function() {
+		this.items = [
+			{
+				text: 'Run'
+			},{
+				text: 'Build'
+			},{
+				text: 'Report'
+			},'-',{
+				xtype: 'container',
+				html: "<div class='cps-title' >Canopsis</div>"
+			},'->',{
+				xtype: 'container',
+				html: "<div class='cps-account' >"+global.account.firstname+" "+global.account.lastname+"</div>",
+				width: 300
+			},'-',{
+				iconCls: 'icon-preferences',
+				menu: {
+					showSeparator: true,
+					items: [
+							{
+								iconCls: 'icon-console',
+								text: 'Show log console',
+								action: 'showconsole'
+							},{
+								iconCls: 'icon-clear',
+								text: 'Clear tabs cache',
+								action: 'cleartabscache'
+							},'-',{
+								iconCls: 'icon-logout',
+								text: 'Logout',
+								action: 'logout'
+							},
+						],
+				}
+			}
+		]
+		this.callParent(arguments);
+	}
+
+});
