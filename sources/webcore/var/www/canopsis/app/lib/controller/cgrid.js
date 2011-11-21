5d74822793c2652144db338c1112a14a7ba8d714
Tristan Carranante <tcarranante@capensis.fr>
2011-11-21 17:04:41 +0100

    cleaning comment

--- sources/webcore/var/www/canopsis/app/lib/controller/cgrid.js
+++ sources/webcore/var/www/canopsis/app/lib/controller/cgrid.js
@@ -324,6 +324,7 @@ Ext.define('canopsis.lib.controller.cgrid', {
 		var store = grid.getStore();
 		var search = grid.down('textfield[name=searchField]').getValue();
 		
+		//create an array and give it to store.search
 		var myArray = []
 		for (i in grid.opt_tbar_search_field){
 			var smth = {}
@@ -331,42 +332,6 @@ Ext.define('canopsis.lib.controller.cgrid', {
 			myArray.push(smth);
 		}
 		store.search(myArray);
-		
-		/*
-		
-		if(search){
-			
-			//creating filter
-			if (grid.opt_tbar_search_field.length == 1){
-				var mfilter = '{"'+ grid.opt_tbar_search_field[0]+'":{ "$regex" : ".*'+search+'.*", "$options" : "i"}}';
-			} else {
-				var mfilter = '{"$or": [';
-				for (i in grid.opt_tbar_search_field){
-					if(i != 0){	mfilter += ',';	}
-					mfilter += '{"'+ grid.opt_tbar_search_field[i]+'":{ "$regex" : ".*'+search+'.*", "$options" : "i"}}';
-				}
-				mfilter += ']}';
-			}
-			//log.debug(mfilter);
-			
-			//adding option to store
-			store.proxy.extraParams = {
-				'filter': mfilter
-			};
-			store.load();
-		}else{
-			store.proxy.extraParams = {};
-			store.load();
-		}
-		
-		log.debug('beforesearch');
-		log.debug(store.proxy.extraParams);
-		log.debug(grid.filter);
-		
-		if (this.searchRecord) {
-			this.searchRecord()
-		}
-		*/
 	}
 	
 });
