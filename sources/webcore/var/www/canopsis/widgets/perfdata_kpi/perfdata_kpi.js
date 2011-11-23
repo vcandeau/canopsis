423089ca3a6d905ce1e220bec8f68c7dad5d570a
Tristan Carranante <tcarranante@capensis.fr>
2011-11-23 17:05:40 +0100

    add function to cwidget + add 2 widget, jgauge and pie

--- sources/webcore/var/www/canopsis/widgets/perfdata_kpi/perfdata_kpi.js
+++ sources/webcore/var/www/canopsis/widgets/perfdata_kpi/perfdata_kpi.js
@@ -25,65 +25,47 @@ Ext.define('widgets.perfdata_kpi.perfdata_kpi' ,{
 			var icon = this.iconset
 		}
 		
-		//calculer % of metric
-		
-		
-		
-		if (data.perf_data_array){
-			var value = data.perf_data_array		
-			if(data.perf_data_array[this.metric]){
-				data = data.perf_data_array[this.metric];
-
-				//give the health , 0% very bad , 100% all clear , and rounded
-				if(data.max){
-					var health = Math.round((100 - (data.value / data.max * 100)) / 10) *10;
-				} else if(data.crit){
-					//take critical if no max value
-					var health = Math.round((100 - (data.value / data.crit * 100)) / 10) *10;
-				} else {
-					log.debug('impossible to calculate health (no max value in data)', this.logAuthor)
-					this.setHtml("<center><div>impossible to calculate health (no max value in data)</br>change it in the view editor</div></center>");
-				}
-				
-				switch (health){
-					case 0:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_0-10'/></center>");
-						break;
-					case 10:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_10-20'/></center>");
-						break;
-					case 20:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_20-30'/></center>");
-						break;
-					case 30:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_30-40'/></center>");
-						break;
-					case 40:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_40-50'/></center>");
-						break;
-					case 50:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_50-60'/></center>");
-						break;
-					case 60:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_60-70'/></center>");
-						break;
-					case 70:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_70-80'/></center>");
-						break;
-					case 80:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_80-90'/></center>");
-						break;
-					case 90:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_90-100'/></center>");
-						break;
-					case 100:
-						this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_90-100'/></center>");
-						break;
-				}
+		var health = this.getHealth(data);
+		if (health){
+			//round the result
+			var roundHealth = Math.round(health / 10) *10;
+			switch (roundHealth){
+				case 0:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_0-10'/></center>");
+					break;
+				case 10:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_10-20'/></center>");
+					break;
+				case 20:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_20-30'/></center>");
+					break;
+				case 30:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_30-40'/></center>");
+					break;
+				case 40:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_40-50'/></center>");
+					break;
+				case 50:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_50-60'/></center>");
+					break;
+				case 60:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_60-70'/></center>");
+					break;
+				case 70:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_70-80'/></center>");
+					break;
+				case 80:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_80-90'/></center>");
+					break;
+				case 90:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_90-100'/></center>");
+					break;
+				case 100:
+					this.setHtml("<center><span class='kpi kpi_iconSet"+icon+"_90-100'/></center>");
+					break;
 			}
-		}else{
-			log.debug('no perfdata', this.logAuthor)
-			this.setHtml("<center><div>No perfdata for the nodeId specified</br>change it in the view editor</div></center>");
+		} else {
+			this.setHtml("<center><div>Metric invalid or data missing, you can set this in the view editor</br>check the console for more details</div></center>");
 		}
 	},
 	
---
+++ sources/webcore/var/www/canopsis/widgets/pie/pie.js
@@ -0,0 +1,111 @@
+Ext.define('widgets.pie.pie' ,{
+	extend: 'canopsis.lib.view.cwidget',
+
+	alias : 'widget.pie',
+	
+	logAuthor: '[pie]',
+	
+	//the pie does note support css color name, so must use the following
+	colors: {
+		up: '#50b432',
+		down: '#ed241b',
+		unreachable: '#f0f0ff',
+		ok : '#50b432',
+		warning: '#ed941b',
+		critical: '#ed241b',
+		unknown: '#f0f0ff' 
+	},
+	
+	initComponent: function() {
+		log.debug('Init pie kpi '+this.id, this.logAuthor)
+		log.debug(' + NodeId: '+ this.nodeId, this.logAuthor)
+		this.callParent(arguments);
+	},
+	
+	onRefresh: function(data){		
+		if(!this.pie){
+			this.setPie();
+		}
+		
+		var value = data.perf_data_array
+		
+		if (value){
+				var values = [];
+				var legend = [];
+				var colors = ['#fff'];
+
+				if(value[this.metric]){
+					var ok = Math.round(value[this.metric].ok);
+					if (ok > 0){
+						values.push(['Ok', ok]);
+						this.options.seriesColors.push(this.colors['ok']);
+						//this.options.seriesColors.push(global.default_colors[15]);
+					}
+					
+					var warn = Math.round(value[this.metric].warn);
+					if (warn > 0){
+						values.push(['Warning', warn]);
+						this.options.seriesColors.push(this.colors['warning']);
+						//this.options.seriesColors.push(global.default_colors[12]);
+					}
+					
+					var crit = Math.round(value[this.metric].crit);
+					if (crit > 0){
+						values.push(['Critical', crit]);
+						this.options.seriesColors.push(this.colors['critical']);
+						//this.options.seriesColors.push(global.default_colors[5]);
+					}
+					
+					var unkn = Math.round(value[this.metric].unkn);
+					if (unkn > 0){
+						values.push(['Unknown', unkn]); 
+						this.options.seriesColors.push(this.colors['unknown']);
+						//this.options.seriesColors.push(global.default_colors[10]);
+					}
+				}
+				if(values.length != 0){
+					if (!this.chart){
+						log.debug('Create the pie '+this.id, this.logAuthor)
+						this.chart = jQuery.jqplot("pie-"+this.id, [values], this.options);
+					}else{
+						log.debug('update the pie '+this.id, this.logAuthor)
+						this.chart.series[0].data = values
+						//this.chart.series[0].color = "#FF0000"
+						this.chart.replot(this.options)
+					}
+				}else{
+					log.debug('Pie cannot be built,no crit/warn/unkn/ok found', this.logAuthor)
+					this.setHtml("<center><div>There is no data to display</br>check if you set the right metric in view editor.</div></center>");
+				}			
+		}else{
+			this.setHtml("<center><div>Impossible to display pie because</br>input data are invalid (check console)</div></center>");
+		}
+	},
+	
+	setPie : function(){
+		this.setHtml("<div id='pie-"+this.id+"' style=height:100%></div>");
+		
+		this.options = {
+						defaultHeight: 100,
+						grid: {
+							borderWidth: 0,
+							shadow: false,
+							background: 'transparent'
+						},
+						seriesDefaults: {
+							renderer: jQuery.jqplot.PieRenderer,
+							rendererOptions: {
+								showDataLabels: true
+							}
+						},
+						legend: {
+							show:true,
+							location: 'e',
+						}
+					}
+		this.options.seriesColors = [];
+	}
+	
+	
+	
+});
