fa433209d1d75a822debf25a737ff95bf8915d10
Benjamin Sans <bsans@capensis.fr>
2012-01-09 14:20:30 +0100

    fix: neb2amqp Makefile + linker

--- sources/neb2amqp/src/config.h
+++ sources/neb2amqp/src/config.h
@@ -132,19 +132,19 @@
 #define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
 
 /* Name of package */
-#define PACKAGE "neb2socket"
+#define PACKAGE "neb2amqp"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "wpain@capensis.fr"
+#define PACKAGE_BUGREPORT "bsans@capensis.fr"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "NEB2Socket"
+#define PACKAGE_NAME "NEB2AMQP"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "NEB2Socket 0.1a"
+#define PACKAGE_STRING "NEB2AMQP 0.1a"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "neb2socket"
+#define PACKAGE_TARNAME "neb2amqp"
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.1a"
