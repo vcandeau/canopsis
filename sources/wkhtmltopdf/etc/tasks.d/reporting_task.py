bf96cb53c505cd6115aa2a0a5e7273e73d6144c2
Geoffrey Lehée <glehee@capensis.fr>
2012-01-10 14:54:25 +0100

    Reporting now use celery task

--- sources/wkhtmltopdf/etc/tasks.d/reporting_task.py
+++ sources/wkhtmltopdf/etc/tasks.d/reporting_task.py
@@ -2,9 +2,9 @@ from celery.task import task
 
 @task
 def render_pdf(filename, viewname, starttime, stoptime, wrapper_conf_file):
+	import os, sys
 	libwkhtml_dir=os.path.expanduser("~/lib")
 	sys.path.append(libwkhtml_dir)
-	
 	try:
 		import wkhtmltopdf.wrapper
 		# Generate config
@@ -16,4 +16,4 @@ def render_pdf(filename, viewname, starttime, stoptime, wrapper_conf_file):
 		# Run rendering
 		return wkhtmltopdf.wrapper.run(settings)
 	except Exception, err:
-		logger.debug(err)
+		print(err)
