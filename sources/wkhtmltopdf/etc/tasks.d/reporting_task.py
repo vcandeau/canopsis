37c1439a1f3dcd8f5e8dac69c766b428ca73ef92
Geoffrey Lehée <glehee@capensis.fr>
2012-01-10 18:34:46 +0100

    Put in grid fs is now a celery task

--- sources/wkhtmltopdf/etc/tasks.d/reporting_task.py
+++ sources/wkhtmltopdf/etc/tasks.d/reporting_task.py
@@ -17,3 +17,17 @@ def render_pdf(filename, viewname, starttime, stoptime, wrapper_conf_file):
 		return wkhtmltopdf.wrapper.run(settings)
 	except Exception, err:
 		print(err)
+
+@task
+def put_in_grid_fs(file_path, ip, port, connection, collection):
+	from pymongo import Connection
+	import gridfs
+	conn=Connection(ip, port)
+	db=conn[connection]
+	fs = gridfs.GridFS(db, collection=collection)
+
+	report_name = file_path.split('/')
+	report_name = report_name[len(report_name)-1]
+	with open(file_path, "r") as report_file:
+		returned_id = fs.put(report_file, content_type="application/pdf", filename=report_name)
+	return returned_id
