54c8a3cb891ab9c7606cd3f3644bf36a1c349ffe
William Pain <wpain@capensis.fr>
2012-01-13 12:20:53 +0100

    Fix empty var

--- sources/build-install.sh
+++ sources/build-install.sh
@@ -356,7 +356,7 @@ fi
 
 if [ $OPT_MPKG -eq 1 ]; then
 	echo "Purge old binaries ..."
-	rm -R $SRC_PATH/../binaries/$P_ARCH || true
+	rm -R $SRC_PATH/../binaries/$ARCH || true
 	rm -R $SRC_PATH/../binaries/noarch || true
 fi
 
