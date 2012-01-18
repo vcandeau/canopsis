3185c5d56094b59c304a07438aa7e15fedff67b9
Tristan Carranante <tcarranante@capensis.fr>
2012-01-18 09:25:43 +0100

    added the ViewBuilder

---
+++ sources/webcore/var/www/canopsis/app/controller/ViewBuilder.js
@@ -0,0 +1,148 @@
+/*
+#--------------------------------
+# Copyright (c) 2011 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+*/
+Ext.define('canopsis.controller.ViewBuilder', {
+	extend: 'canopsis.lib.controller.cgrid',
+    
+	views: ['ViewBuilder.View', 'ViewBuilder.Form'],
+	stores: ['View', 'Widget'],
+	models: ['view', 'widget'],
+	logAuthor: '[controller][ViewBuilder]',
+
+	iconCls: 'icon-crecord_type-view',
+
+	init: function() {
+		log.debug('Initialize ...',this.logAuthor);
+
+		this.formXtype = 'ViewBuilderForm'
+		this.listXtype = 'ViewBuilder'
+
+		this.modelId = _('view')
+		
+		this.widgetCounter = 0
+
+		this.callParent(arguments);
+	},
+	
+	_addButton: function(button) {
+		log.debug('clicked addButton',this.logAuthor);
+		log.debug("Create tab '"+this.formXtype+"'",this.logAuthor)
+		
+		this.form = add_view_tab('ViewBuilderForm', '*'+ _('New') +' '+this.modelId, true, undefined, true, false, false)
+	/*	$(function() {
+                $( "#sortable" ).sortable({
+						placeholder: "ui-sortable-placeholder",
+						//forcePlaceholderSize: false,
+						opacity: 0.4,
+						start: function(e, ui){
+							ui.placeholder.height(ui.item.height());
+							ui.placeholder.width(ui.item.width());
+						}
+					});
+                $( "#sortable" ).disableSelection();
+        });*/
+        
+        $(function() {
+			$("#container").jqGridable()
+         });
+		this._bindFormEvents(this.form)
+		
+	},
+	
+/*	_contextMenu : function(view, rec, node, index, e) {
+		log.debug('Show context menu',this.logAuthor);
+		e.stopEvent();
+		this.form.contextMenu.showAt(e.getXY());
+    },*/
+	
+	_bindFormEvents: function(form){
+		log.debug('Binding WYSIWYG editor',this.logAuthor);
+	
+		form.saveButton.on('click', this._saveForm,this)
+		form.addWidgetButton.on('click', this.addWidget,this)
+	},
+	
+	addWidget : function(){
+		//this.widgetCounter
+		
+		$('#sortable').append('<li id="l'+this.widgetCounter+'"><div id="b'+this.widgetCounter+'" class="ui-widget-content" style="width:100%;height:100%;">box '+this.widgetCounter+'</div></li>');
+        $('#l'+this.widgetCounter).resizable({
+                        grid: 50,
+						minHeight: 100,
+						minWidth: 100,
+						handles: 'se',
+						//stop: this.checkWidgetSize
+        });
+        this.widgetCounter++
+	},
+	
+	checkWidgetSize : function(event,ui){
+		//log.debug($('#'+ui.element.context.id).get())
+		var width = $(this).width()
+		var height = $(this).height()
+		
+		//if not right size, resize it
+		if(((width%50) != 0) && ((height%50) != 0)){
+			var new_width = Math.round(width/50) * 50
+			var new_height = Math.round(height/50) * 50
+			
+			log.debug(new_width)
+			log.debug(new_height)
+			
+			if(new_width > 100){
+				new_width += ((new_width / 100)-1) * 3
+			}
+			if(new_height > 100){
+				new_height += ((new_height / 100)-1) * 6
+			}
+			
+			//log.debug(new_width)
+			//log.debug(new_height)
+			
+			$(this).css({
+					width: new_width,
+					height: new_height
+			})
+		}
+	},
+	
+	_addDropZone: function(){
+		
+	},
+	
+	_saveForm : function(form){
+		
+	},
+
+	beforeload_EditForm: function(form){
+
+	},
+	
+
+	afterload_EditForm: function(form){
+
+	},
+	
+	afterload_DuplicateForm: function(form){
+
+	}
+
+
+});
---
+++ sources/webcore/var/www/canopsis/app/view/ViewBuilder/Form.js
@@ -0,0 +1,134 @@
+/*
+#--------------------------------
+# Copyright (c) 2011 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+*/
+Ext.define('canopsis.view.ViewBuilder.Form' ,{
+	extend: 'Ext.panel.Panel',
+
+	alias : 'widget.ViewBuilderForm',
+	
+	border: 0,
+	
+	layout : 'fit',
+	
+	autoScroll: true,
+
+	tbar: [{
+		iconCls: 'icon-save',
+		text: _('Save'),
+		action : 'save',
+	},{
+		iconCls: 'icon-cancel',
+		text: _('Cancel'),
+		action: 'cancel',
+	}
+	],
+	
+	//-----------------------------------------------------------------------------------
+	
+	initComponent: function() {
+		this.on('beforeclose', this.beforeclose)
+		this.on('afterrender', this.afterrender, this)
+		
+		//-----------top bar------------
+		this.saveButton = Ext.create('Ext.Button', {
+								iconCls: 'icon-save',
+								text: _('Save'),
+								action : 'save',
+							})
+							
+		this.cancelButton = Ext.create('Ext.Button', {
+								iconCls: 'icon-cancel',
+								text: _('Cancel'),
+								action: 'cancel',
+							})
+							
+		this.addWidgetButton = Ext.create('Ext.Button', {
+								iconCls: 'icon-add',
+								text: _('Add'),
+								action: 'addWidget',
+							})
+		
+		this.tbar =  [this.saveButton,this.cancelButton,'->',this.addWidgetButton],
+		
+		//-------Context menu------
+	/*	this.widgetAction = Ext.create('Ext.Action', {
+								//iconCls: 'icon-delete',
+								text: _('Widget'),
+							})
+
+		this.contextMenu = Ext.create('Ext.menu.Menu',{
+								items : [this.widgetAction],
+							});*/
+		
+		this.callParent(arguments);
+		
+		//------jquery masonry------
+		//var contentLi = new Ext.Element('<ul id="sortable"></ul>')
+		//log.dump(contentLi)
+		this.masonryLi = this.add({
+			xtype: 'container',
+			id: 'container',
+		})
+		
+		
+		
+		log.debug('Form finish generating')
+	},
+
+	afterrender: function() {
+	
+
+	},
+	
+	
+	//-------------------------------cgrid inherited function----------------------------
+	loadRecord: function(record){
+	/*	widgets =  record.data.items;
+		this.GlobalOptions.down('textfield[name=crecord_name]').setValue(record.get('crecord_name'));
+		this.GlobalOptions.down('numberfield[name=refreshInterval]').setValue(record.get('refreshInterval'));
+		this.GlobalOptions.down('numberfield[name=nbColumns]').setValue(record.get('nbColumns'));
+		this.GlobalOptions.down('numberfield[name=rowHeight]').setValue(record.get('rowHeight'));
+		//needed for loading node, cf ViewEditor.js controller, beforeload_EditForm function
+		this.nodeId = record.get('nodeId');
+		
+		for (i in widgets){
+					copy = Ext.ClassManager.instantiate('canopsis.model.widget',widgets[i]);
+					this.ItemsStore.add(copy);
+		}
+*/
+	},
+
+	//------------------------------------------------------------------------------------
+
+	beforeclose: function(tab, object){
+		log.debug('[ViewEditor][cform] - Active previous tab');
+		old_tab = Ext.getCmp('main-tabs').old_tab;
+		if (old_tab) {
+			Ext.getCmp('main-tabs').setActiveTab(old_tab);
+		}
+	},
+
+	beforeDestroy : function() {
+		log.debug("Destroy items ...")
+		canopsis.view.Tabs.Content.superclass.beforeDestroy.call(this);
+		log.debug(this.id + " Destroyed.")
+	},
+	
+});
