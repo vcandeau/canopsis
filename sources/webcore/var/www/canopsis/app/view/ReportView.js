38b5174d307fc933db4d0cace3770ec9efcf47cb
Tristan Carranante <tcarranante@capensis.fr>
2012-02-02 14:23:20 +0100

    reporting working with jqgridable

--- sources/webcore/var/www/canopsis/app/view/ReportView.js
+++ sources/webcore/var/www/canopsis/app/view/ReportView.js
@@ -5,138 +5,27 @@ Ext.define('canopsis.view.ReportView', {
 	setContent : function(){
 		//----------------Setting globals options----------------
 		var items = this.view.items;
-		var totalWidth = this.getWidth() - 20
-		var nodeId = this.view.nodeId
-		var refreshInterval = this.view.refreshInterval
-		var nbColumns = this.view.nbColumns
-		var rowHeight = this.view.rowHeight
-		
-		if (! rowHeight) { rowHeight = 200 }
-		if (! refreshInterval) { refreshInterval = 0 }
-		if (! nbColumns || items.length == 1) { nbColumns = 1 }
-		
-		this.layout.columns = nbColumns
-		/*
-		log.debug('---------------------------------------')
-		log.dump(export_to)
-		log.dump(export_from)
-		log.dump(export_view_id)
-		log.debug('---------------------------------------')
-		*/
-		//---------------Adding presentation widget-------------------
-		//get information
-		/*
-		var presentationData = [];
-		presentationData['name'] = 'Canopsis';
-		presentationData['startDate'] = Ext.Date.format(new Date(reportStart),'Y/m/d');
-		presentationData['stopDate'] = Ext.Date.format(new Date(reportStop),'Y/m/d');
-		
-		//create tpl
-		var presentationText = '<font size="9" face="verdana">      {name} reporting</font><br><br><center>From {startDate} to {stopDate}</center><br/><br/><br/>'
-		var presentationTemplate = new Ext.Template(presentationText, {compiled: true})
-		
-		//create widget
-		var presentationWidget = Ext.create('canopsis.lib.view.cwidget',{
-			colspan : nbColumns,
-		})
-		this.add(presentationWidget);
-		
-		
-		//setHtml
-		presentationWidget.setHtmlTpl(presentationTemplate,presentationData);
-		*/
-		
-		//-------------------page height-----------------------
-	/*	var column_cpt = 1
-		var widget_height = 0
-		var docHeight = 0
-		
-		//for each widget
-		for(var i= 0; i < items.length; i++) {
-			var item = items[i]
-			if (! item.rowHeight) { item.rowHeight=rowHeight }
-			
-			log.debug('looping into the loop')
-			//if first widget, just set the Height
-			if(column_cpt == 1){
-				log.debug('first widget')
-				widget_height = item.rowHeight
-				log.debug('widgetrowheight is:' + item.rowHeight)
-			} else {
-				//else compare widget Height with Height of previous widget
-				if(widget_height < items[i].rowHeight){
-					log.debug('comparing rowHeight')
-					widget_height = item.rowHeight
-				}
-			}
-			
-			//if end of column, add height to docHeight
-			if(column_cpt == nbColumns){
-				docHeight += widget_height;
-				widget_height = 0
-				column_cpt = 1
-				log.debug('new doHeight')
-				log.dump(docHeight)
-			} else {
-				log.debug('column cpt + 1')
-				column_cpt++
-			}
-		}
-		*/
-		//var docHeight = Math.ceil(items.length / nbColumns) * 200 +100
-	/*	if(docHeight < 1200){
-			document.body.style.height = 1200
-		}else{*/
-			//document.body.style.height = docHeight + 100
-		//}
-		//this.height = docHeight
-		//this.width = 840
-		
-		/*
-		var page_height = 900;
-		var widget_per_page = 12;
-		var nb_page = items.length / 12
-		
-		if(nb_page > 1){
-			nb_page = Math.ceil(nb_page)
-			document.body.style.height = Math.ceil(items.length / nbColumns) * 200 
-		} 
-		
-		/*
-		//var widget_per_page= Math.round(page_height / rowHeight);
-		this.height = 8000
-		//this.height = Math.ceil(items.length / nbColumns) * 200
-		log.debug('----------------------------------------------------------------------')
-		log.dump(this.height)
-		log.debug('----------------------------------------------------------------------')
-		*/
-		
-		//-----------------populating with widgets--------------
-		//fixing layout (table goes wild without it)
-	/*	for (i; i<nbColumns; i++){
-			this.add({ html: '', border: 0, height: 0, padding:0})
-		}*/
-		
 
-		var ext_items = []
+		if (items.length == 1 ) {
+			this.set_one_item(items)
+		} else {
+			this.set_many_items(items)
+		}
+
+		/*
 		for(var i= 0; i < items.length; i++) {
 				
 			log.debug(' - Item '+i+':', this.logAuthor)
 			var item = items[i]
+			if(item.data){
+				item = item.data
+			}
 
 			log.debug('   + Add: '+item.xtype, this.logAuthor)
 
 			item['mytab'] = this
-			item['fullmode'] = false
-
-			var colspan = 1
-			var rowspan = 1
-
-			if (item['colspan']) { colspan = item['colspan'] }
-			if (item['rowspan']) { rowspan = item['rowspan'] }
 			
-			item['width'] = (totalWidth / nbColumns) * colspan
-			item['style'] = {padding: '3px'}
+			item['width'] = '100%'
 			item['border'] = false
 
 			item['reportMode'] = true;
@@ -154,15 +43,78 @@ Ext.define('canopsis.view.ReportView', {
 			if (! item.rowHeight) { item.height=rowHeight }else{ item.height=item.rowHeight }
 			if (item.title){ item.border = true }
 
+
 			this.add(item);
 			//log.debug(item);
 		}
-		document.body.style.height = this.getHeight() + 100
+		* */
+		var height = $('#' +this.jqgridable.id).height()
+		document.body.style.height = height + 100
+		this.doLayout()
 		
 		var task = new Ext.util.DelayedTask(function(){
 			window.status = 'ready'
 		});
 		task.delay(3000)
 	},
+		
+	set_one_item : function(items){
+		log.debug(' + Use full mode ...', this.logAuthor)
+		this.layout = 'fit'
+		
+		//check if standart item view or jqgridable item
+		var item = items[0]
+		if(item.data){
+			item = item.data
+		}
+
+		log.debug('   + Add: '+item.xtype, this.logAuthor)
+
+		item['width'] = '100%'
+		item['title'] = ''
+		item['mytab'] = this
+
+		item['reportMode'] = true;
+		item['exportMode'] = true;
+		
+		item.export_from = export_from
+		item.export_to = export_to
+
+		//Set default options
+		//if (! item.nodeId) { item.nodeId=nodeId}
+		//if (! item.refreshInterval) { item.refreshInterval=refreshInterval}
+
+		this.add(item)
+	},
+	
+	set_many_items : function(items){
+		//setting jqgridable
+		this.jqgridable = Ext.create('canopsis.view.Tabs.JqGridableViewer',{view_widgets : items})
+		this.add(this.jqgridable)
+		
+		this.jqgridable._load(items)
+		
+		var widget_list = this.jqgridable.get_ext_widget_list()
+		
+		//here processing on data
+		
+		//globalNodeId
+		//refresh interval
+		
+		//////////////////////////
+		
+		for(var i in widget_list){
+			var item = items[i].data
+			
+			item['reportMode'] = true;
+			item['exportMode'] = true;
+			
+			item.export_from = export_from
+			item.export_to = export_to
+			
+			widget_list[i].add(item)
+		}
+	}	
+		
 		
 });
