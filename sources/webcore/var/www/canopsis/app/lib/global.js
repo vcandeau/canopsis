3e68151951b98f993a467e71c18fd64fca719231
William Pain <wpain@capensis.fr>
2012-02-15 11:00:38 +0100

    Set verbose

--- sources/webcore/var/www/canopsis/app/lib/global.js
+++ sources/webcore/var/www/canopsis/app/lib/global.js
@@ -30,7 +30,7 @@ var global = {
 	},
 	log: {
 		/* 0: none, 1: info, 2: error, 3: error + warning, 4: error + warning + debug, 5: error + warning + debug + dump */
-		level: 0,
+		level: 5,
 		buffer: 50
 	},
 
