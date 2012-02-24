6f0aeb1538654946af775bb223c6ede1287dd283
William Pain <wpain@capensis.fr>
2012-02-24 17:23:33 +0100

    New cinventory

--- sources/webcore/var/www/canopsis/app/lib/form/field/cinventory.js
+++ sources/webcore/var/www/canopsis/app/lib/form/field/cinventory.js
@@ -18,36 +18,52 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 */
+
 Ext.define('canopsis.lib.form.field.cinventory' ,{
 	extend: 'Ext.panel.Panel',
 	
 	alias: 'widget.cinventory',
-
-	window: false,
+	
+	border: false,
+	
+	metrics: true,
 	multiSelect: true,
-	ids: false,
 	
-	layout: {
-				type: 'hbox',
-				align: 'stretch',
-			},
-	
-	last_search : '',
-	defaults: { padding: 5 },
-	search_type : 'all',
-	search_source_type : 'all',
-	prefetch_id : '',
-	
-	height : 300,
-	
-	border : false,
-
 	initComponent: function() {
-		log.debug('[cinventory] - Initialize ...')
+		this.logAuthor = '[' + this.id + ']'
+		log.debug('Initialize ...', this.logAuthor)
+		
+		var default_layout = { type: 'hbox', align: 'stretch'}
+		var default_defaults = { padding: 5 }
+		
+		this.items = this.buildView();
+		
+		if (! this.multiSelect){
+			default_layout.type = 'vbox'
+		}
+		
+		//HACK: for test in widget, wrap in container
+		if (this.height){
+			log.debug(' + Widget mode', this.logAuthor)
+			this.items = {xtype: 'container', items: this.items, layout: default_layout, defaults: default_defaults}
+			this.layout = 'fit'
+		}else{
+			this.layout = default_layout
+			this.defaults = default_defaults
+		}
+		
+		this.callParent(arguments);
+	},
 
+	buildView: function(){
+		log.debug(' + Build view ...', this.logAuthor)
+		
+		var items = []
+		
+		// Configuration
 		var model = Ext.ModelManager.getModel('canopsis.model.event');
-
-		this.Win_columns = [{
+		
+		this.columns = [{
 					header: '',
 					width: 25,
 					sortable: false,
@@ -68,13 +84,137 @@ Ext.define('canopsis.lib.form.field.cinventory' ,{
 					flex: 2,
 					dataIndex: 'resource',
 		}];
-
-		this.columns = this.Win_columns
-
-		//------------------- create stores---------------
-		this.InventoryStore = Ext.create('canopsis.lib.store.cstore', {
+		
+		//////// Selection GRID
+		log.debug(' + Selection grid', this.logAuthor)
+		
+		this.selection_render =  function(value, p, record){
+			var node = ''
+			if (record.data.resource){
+				node = Ext.String.format('<b>{0}</b><br>&nbsp;&nbsp;{1}', record.data.component,record.data.resource );
+			}else{
+				node = Ext.String.format('<b>{0}</b>', record.data.component );
+			}
+			return node
+		}
+		
+		this.selection_render_metrics = function(perf_data, p, record){
+			if (perf_data) {
+				var output = ''
+								
+				for (var name in perf_data){
+					metric = perf_data[name]
+					
+					var checked = 'checked'
+					var state = this.get_metric(record.data.id, name)
+					if (! state){ checked ='' }
+					
+					output += Ext.String.format(
+						'<input type="checkbox" value="{0}" onclick="Ext.getCmp(\'{1}\').check_metric(\'{2}\',\'{3}\');" {4}> {5}<br>',
+						name,
+						this.id,
+						record.data.id,
+						name,
+						checked,
+						name
+					);
+				}
+				return output
+			}
+		}
+		
+		this.selection_store = Ext.create('Ext.data.Store', {
 				model: model,
-				pageSize: 10,
+		});
+		
+		// If metric mode
+		var columns_metrics = {}
+		if (this.metrics){
+			columns_metrics = {
+					sortable: false,
+					dataIndex: 'perf_data_array',
+					flex: 1,
+					renderer: this.selection_render_metrics,
+				}
+		}
+		
+		var selection_height = undefined
+		var selection_flex = 1
+		if (! this.multiSelect){
+			selection_flex = undefined
+			selection_height = 130
+		}
+		
+		this.selection_grid = Ext.create('canopsis.lib.view.cgrid', {
+			title: _("Selection"),
+			multiSelect: this.multiSelect,
+			opt_bar: false,
+			border: true,
+			opt_paging: false,
+			flex : selection_flex,
+			height: selection_height,
+			store: this.selection_store,
+			hideHeaders: true,
+			autoScroll: true,
+			columns: [
+				{
+					header: '',
+					width: 25,
+					sortable: false,
+					dataIndex: 'source_type',
+					renderer: rdr_source_type
+	       		},{
+					sortable: false,
+					dataIndex: 'id',
+					flex: 2,
+					renderer: this.selection_render
+	       		}, columns_metrics
+			],
+			
+			viewConfig: {
+				plugins: {
+					ptype: 'gridviewdragdrop',
+					enableDrag: false,
+					dropGroup: 'search_grid_DNDGroup'
+				},
+			},
+			
+			// keep checked metrics
+			metrics: {},
+			init_metric: function(node, metric) {
+				if (this.metrics[node] == undefined){
+					this.metrics[node] = {}
+				}
+				
+				if (this.metrics[node][metric] == undefined){
+					this.metrics[node][metric] = true
+				}
+			},
+			
+			check_metric: function(node, metric, check) {
+				
+				this.init_metric(node, metric)
+				
+				if (check == undefined){
+					//toggle
+					check = ! this.metrics[node][metric]
+				}
+				this.metrics[node][metric] = check
+				
+				return check
+			},
+			
+			get_metric: function(node, metric) {
+				this.init_metric(node, metric)
+				return this.metrics[node][metric]
+			}
+		});
+		
+		//////// Search GRID
+		log.debug(' + Search grid', this.logAuthor)
+		this.search_store = Ext.create('canopsis.lib.store.cstore', {
+				model: model,
+				pageSize: 15,
 				proxy: {
 					type: 'rest',
 					url: '/rest/events/event',
@@ -84,291 +224,174 @@ Ext.define('canopsis.lib.form.field.cinventory' ,{
 						totalProperty  : 'total',
 						successProperty: 'success'
 					},
-				/*	writer: {
-						type: 'json',
-						writeAllFields: false,
-					},*/
 				},
+				
+				autoLoad: true
 		})
 		
-		this.store = Ext.create('Ext.data.Store', {
-				model: model,
-		});
-
-		this.LoadStore(this.ids)
-
-		this.callParent(arguments);
-		
-		this.createForm()
-		
-		this.relayEvents(this.store, ['datachanged'])
-	},
-	
-	afterRender : function(){
-		this.callParent(arguments);
-		this.KeyNav = Ext.create('Ext.util.KeyNav', this.id, {
-			scope: this,
-			enter: this.searchFunction
-		});
-	},
-
-	LoadStore: function(ids) {
-		//load list of ids in store
-		if (ids){
-			// format url argument
-			var i;
-			var ids_txt="";
-			for (i in ids){
-				if (ids[i]){
-					ids_txt = ids_txt + "," + ids[i]
-				}
-			}
-			if (ids_txt){
-				// request
-				Ext.Ajax.request({
-					url: '/rest/events/state/' + ids_txt,
-					scope: this,
-					success: function(response){
-						var data = Ext.JSON.decode(response.responseText)
-						data = data.data
-						if (data){
-							if (this.multiSelect){
-								var i;
-								for (i in data){
-									this.store.add(Ext.create('canopsis.model.event', data[i]))
-								}
-							}else{
-								this.store.add(Ext.create('canopsis.model.event', data[0]))
-							}
-						}
-					},
-					failure: function ( result, request) {
-						log.debug('Ajax request failed')
-					} 
-				})
-			}
-		}
-	},
-
-	createForm: function(field, options){
-
-		//--------------------stores-----------------
-		
-		var comboSourceTypeStore = Ext.create('Ext.data.Store', {
-			fields: ['name'],
-			data : [
-				{"name":"all"},
-				{"name":"component"},
-				{"name":"resource"}
-			]
-		});
-
-		var comboTypeStore = Ext.create('Ext.data.Store', {
-			fields: ['name'],
-			data : [
-				{"name":"all"},
-				{"name":"check"},
-			]
-		});
-		
-		//-----------------------grids--------------------
-		this.firstGrid = Ext.create('canopsis.lib.view.cgrid', {
+		this.search_grid = Ext.create('canopsis.lib.view.cgrid', {
 			multiSelect: this.multiSelect,
-			opt_bar: false,
+			opt_bar: true,
+			opt_bar_search: true,
+			opt_bar_add: false,
+			opt_bar_duplicate: false,
+			opt_bar_reload: true,
+			opt_bar_delete: false,			
+			opt_bar_search_field: ['_id' ],
 			border: true,
+			opt_paging: true,
+			multiSelect: this.multiSelect,
 			flex : 1,
+			store: this.search_store,
+			columns: this.columns,
 			viewConfig: {
 				copy : true,
 				plugins: {
 					ptype: 'gridviewdragdrop',
-					dragGroup: 'firstGridDDGroup',
+					enableDrop: false,
+					dragGroup: 'search_grid_DNDGroup',
 				},
 			},
-			store: this.InventoryStore,
-			columns: this.Win_columns,
-			stripeRows: true,
-			title: _('Inventory'),
 		});
 		
+		//////// Bind cgrid controller on search grid
+		this.search_ctrl = Ext.create('canopsis.lib.controller.cgrid');
+		
+		this.on('afterrender', function() {
+			this.search_ctrl._bindGridEvents(this.search_grid)	
+		}, this);
+		
+		//////// Bind events
+		log.debug(' + Bind events', this.logAuthor)
+		
+		this.selection_grid.on('itemdblclick',function(grid, record, item, index){
+			this.selection_store.removeAt(index)
+			this.selection_grid.metrics[record.data.id] = undefined
+		}, this);
 
-		this.firstGrid.on('itemdblclick',function(grid, record, item, index){
+		this.search_grid.on('itemdblclick',function(grid, record, item, index){
+			this.addRecord(record)
+		}, this);
+
+		this.selection_grid.getView().on('beforedrop', function(event, data, dropRec, dropPosition) {
+			var records = data.records
+			for (var i in records){
+				var record = records[i]
+				this.addRecord(record)
+			}
+			
+			event.cancel = true;
+			event.dropStatus = true;
+			
+			
+			return false
+		}, this);
+		
+		//////// Push items
+		log.debug(' + Set items', this.logAuthor)
+		items.push(this.search_grid)
+		items.push(this.selection_grid)
+		
+		return items
+	},
+	
+	addRecord : function(record, index){		
+		if (this.selection_store.find('id', record.data.id) == -1 ){
+			
 			if (! this.multiSelect){
-				this.store.removeAt(0)
+				this.selection_store.removeAll()
 			}
-			this.store.add(record)
-		}, this);
-
-		var secondGrid = Ext.create('canopsis.lib.view.cgrid', {
-			multiSelect: this.multiSelect,
-			opt_bar: false,
-			border: true,
-			//width : 
-			opt_paging: false,
-			flex : 1,
-			viewConfig: {
-				copy : true,
-				plugins: {
-					ptype: 'gridviewdragdrop',
-					dropGroup: 'firstGridDDGroup'
-				},
-				listeners: {
-					drop: function(node, data, dropRec, dropPosition) {
-						if (! this.multiSelect){
-							if (dropPosition == 'after'){
-								this.store.removeAt(0)
-							}else{
-								this.store.removeAt(1)
-							}
-						}
-					}
-				},
-			},		
-			store: this.store,
-			columns: this.Win_columns,
-			stripeRows: true,
-			//title: _('Selection'),
-		});
-
-		secondGrid.on('itemdblclick',function(grid, record, item, index){
-			this.store.removeAt(index)
-		}, this);
-
-		this.on('itemdblclick',function(grid, record, item, index){
-			this.store.removeAt(index)
-		}, this);
-
-		//------------------Search Options-------------------
-
-		comboSourceType = Ext.create('Ext.form.ComboBox', {
-			fieldLabel: _('Source type'),
-			store: comboSourceTypeStore,
-			queryMode: 'local',
-			displayField: 'name',
-			forceSelection: true,
-			editable: false,
-			value: this.search_source_type,
-			name : 'source_type'
-		});
-
-		comboType = Ext.create('Ext.form.ComboBox', {
-			fieldLabel: _('Type'),
-			store: comboTypeStore,
-			queryMode: 'local',
-			displayField: 'name',
-			forceSelection: true,
-			editable: false,
-			value: this.search_type,
-			name : 'type'
-		});
-
-		this.searchForm = Ext.create('Ext.form.Panel', {
-			border: 0,
-			buttonAlign: 'right',
-			defaultType: 'textfield',
-			//bodyStyle: 'padding: 5px;',
-			//height: 100,
-			//flex : 1,
-			items: [{
-				fieldLabel: _('Search'),
-				name: 'search',
-				//allowBlank: false,
-			}],
-		});
-		
-		var searchButton = Ext.create('Ext.Button', {
-			text: _('Search'),
-		});
-
-		this.searchForm.add(comboSourceType);
-		this.searchForm.add(comboType);
-		this.searchForm.add(searchButton);
-
-		//----------------building dislay pannel---------------
-
-		this.displayPanel = Ext.create('Ext.Panel', {
-			flex : 1,
-			layout: {
-				type: 'vbox',
-				align: 'stretch',
-				padding : '5 0 0 0',
-			}, 
-			border : false,
-			items : [this.searchForm,secondGrid]
-		})
-
-		this.add([this.displayPanel, this.firstGrid])
-
-		
-		//---------------diplay and launch prefetch--------------------
-
-		this.prefetch();
-		//this.down('textfield[name="search"]').focus('', 700); 
-
-		//------------------------binding------------------------------
-
-		searchButton.on('click',this.searchFunction, this);
-	},
-	
-
-
-	beforeDestroy : function() {
-		Ext.grid.Panel.superclass.beforeDestroy.call(this);
-	        log.debug("[cinventory] " + this.id + " Destroyed.")
-	},
-
-	prefetch : function(){
-		if (this.prefetch_id != ''){
-			this.searchForm.down('textfield[name="search"]').setValue(this.prefetch_id);
-			this.searchFunction();
-		}
-	},
-	
-	searchFunction : function(){
-		var form = this.searchForm.getForm()
-		if (form.isValid()){
-			var values = form.getValues();
-			//clean searching values
-			var search = values.search
-
-			log.debug('[cinventory] Search: '+search);
-
-			var filter = {};
-
-			if (search){
-				if (search == '*'){
-					filter["_id"] = { "$regex" : ".*" };
-				}else{
-					filter["_id"] = { "$regex" : ".*"+search+".*", "$options" : "i"};
-				}
+			
+			if (index != undefined){
+				this.selection_store.insert(index, record.data)
+			}else{
+				this.selection_store.add(record.data)
 			}
-
-			if (values.source_type != 'all'){
-				filter["source_type"] = values.source_type;
-			}			
-			if (values.type != 'all'){
-				filter["type"] = values.type;
-			}
-
-			log.debug("[cinventory] Filter:");
-			log.dump(filter);
-
-			this.firstGrid.pagingbar.moveFirst();
-			this.InventoryStore.search(filter);
+		}else{
+			log.debug(record.data.id + " already selected", this.logAuthor)
 		}
 	},
 
+	// GetValue for wizard ...
 	getValue : function(){
-		var record = this.store.getAt(0)
-		if(record){
-			return record.get('id')
-		} else {
-			return undefined
-		}
+		var dump = []
+		var me = this
+		
+		this.selection_store.each(function(record){
+			var id = record.data.id
+			
+			var metrics = []
+			
+			if (me.metrics) {
+				var metrics_checked = me.selection_grid.metrics[id]
+				var nb_metric = 0
+				
+				for (var metric in metrics_checked){
+					if (metrics_checked[metric]){
+						metrics.push(metric)
+					}
+					nb_metric += 1
+				}
+				
+				//check if all metrics are checked
+				/*if (metrics.length == nb_metric){
+					rdump.metrics = [ "<all>" ]
+				}*/
+			}
+			
+			console.log(nb_metric)
+			console.log(metrics)
+			
+			var rdump = {
+				id: id,
+				component: record.data.component,
+				perf_data_array: record.data.perf_data_array,
+				resource: record.data.resource,
+				source_type: record.data.source_type,
+				metrics: [],
+			}
+			
+			//console.log(rdump.metrics.length)
+			console.log(rdump.metrics)
+			console.log(rdump)
+			console.log(rdump.metrics)
+			//console.log(rdump.metrics.length)
+			dump.push(rdump)
+		});
+		
+		log.debug("getValue Dump:", this.logAuthor)
+		log.dump(dump)
+		
+		return dump
 	},
 
 	setValue : function(data){
-		var tab = [data]
-		this.LoadStore(tab)
-	}
+		log.debug("setValue Data:", this.logAuthor)
+		log.dump(data)
+
+		for (var i in data){
+			var rdata = data[i]
+			for (var metric in rdata.perf_data_array){
+				if (metric != '<all>')
+					this.selection_grid.check_metric(rdata.id, metric , false)
+			}
+			for (var j in rdata.metrics){
+				var metric = rdata.metrics[j]
+				if (metric != '<all>')
+					this.selection_grid.check_metric(rdata.id, metric, true)
+			}
+			this.addRecord({data: rdata})
+		}
+	},
+	
+	beforeDestroy : function() {
+		this.search_ctrl.destroy()
+		
+		this.selection_grid.destroy()
+		this.search_grid.destroy()
+		
+		Ext.grid.Panel.superclass.beforeDestroy.call(this);
+		log.debug("Destroyed.", this.logAuthor)
+	},
 
 });
