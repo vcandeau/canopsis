f0cd8cb25ddbf5055b6e3bf5b9eb69b72fc0a74a
Tristan Carranante <tcarranante@capensis.fr>
2012-03-01 10:55:45 +0100

    little modifications

--- sources/webcore/var/www/canopsis/app/store/TreeStoreView.js
+++ sources/webcore/var/www/canopsis/app/store/TreeStoreView.js
@@ -25,11 +25,16 @@ Ext.define('canopsis.store.TreeStoreView', {
 	
 	storeId: 'store.TreeStoreView',
 	
-	clearOnLoad: true,
+	autoLoad : true,
+	autoSync : false,
 	
+	clearOnLoad: true,
+	/*
 	root:{
 		id:'directory.root'
 	},
+	*/
+	defaultRootId : 'directory.root',
 	
 	proxy: {
 			type: 'rest',
@@ -47,43 +52,54 @@ Ext.define('canopsis.store.TreeStoreView', {
 		listeners: {
 			move: function( node, oldParent, newParent, index, options ) {
 				this.sync();
-			}
+			},
 		},
 		
 	//--------------FIX , because without this in 4.0.7 you can't refresh !--------
 	//http://www.sencha.com/forum/showthread.php?151674-4.0.7-Tree-Store-Bug&highlight=treestore
-	//
-    load: function(options) {
-        options = options || {};
-        options.params = options.params || {};
+	//http://www.sencha.com/forum/showthread.php?151211-Reloading-TreeStore-adds-all-records-to-store-getRemovedRecords
+   load : function(options) {
+		options = options || {};
+		options.params = options.params || {};
 
-        var me = this,
-            node = options.node || me.tree.getRootNode(),
-            root;
+		var me = this, node = options.node || me.tree.getRootNode(), root;
 
-        // If there is not a node it means the user hasnt defined a rootnode yet. In this case lets just
-        // create one for them.
-        if (!node) {
-            node = me.setRootNode({
-                expanded: true
-            });
-        }
+		// If there is not a node it means the user hasnt defined a
+		// rootnode yet. In this case lets just
+		// create one for them.
+		if (!node) {
+			node = me.setRootNode({
+						expanded : true
+					});
+		}
 
-        if (me.clearOnLoad) {
-            //BUG
-            //node.removeAll(true);
-            node.removeAll(false);
-        }
+		// copied from 4.1.0.BETA to fix delete calls to the proxy for the remove element.
+		if (me.clearOnLoad) {
+			if (me.clearRemovedOnLoad) {
+				// clear from the removed array any nodes that were
+				// descendants of the node being reloaded so that they
+				// do not get saved on next sync.
+				me.clearRemoved(node);
+			}
+			// temporarily remove the onNodeRemove event listener so
+			// that when removeAll is called, the removed nodes do not
+			// get added to the removed array
+			me.tree.un('remove', me.onNodeRemove, me);
+			// remove all the nodes
+			node.removeAll(false);
+			// reattach the onNodeRemove listener
+			me.tree.on('remove', me.onNodeRemove, me);
+		}
 
-        Ext.applyIf(options, {
-            node: node
-        });
-        options.params[me.nodeParam] = node ? node.getId() : 'root';
+		Ext.applyIf(options, {
+					node : node
+				});
+		options.params[me.nodeParam] = node ? node.getId() : 'root';
 
-        if (node) {
-            node.set('loading', true);
-        }
+		if (node) {
+			node.set('loading', true);
+		}
 
-        return me.callParent([options]);
-    }
+		return me.callParent([options]);
+	}
 });
