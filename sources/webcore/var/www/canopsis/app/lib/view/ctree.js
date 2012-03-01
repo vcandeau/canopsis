c6b263dbffce7221cd6a16dd3a8cec786d532028
Tristan Carranante <tcarranante@capensis.fr>
2012-03-01 15:31:05 +0100

    fix : when close tree it doesn't re open

--- sources/webcore/var/www/canopsis/app/lib/view/ctree.js
+++ sources/webcore/var/www/canopsis/app/lib/view/ctree.js
@@ -35,13 +35,14 @@ Ext.define('canopsis.lib.view.ctree' ,{
 	opt_bar_delete:true,
 	opt_bar_duplicate: false,
 	opt_bar_reload: true,
-
+/*
 	viewConfig: {
 		plugins: [{
-			ptype: 'treeviewdragdrop'
+			ptype: 'treeviewdragdrop',
+			pluginId : 'ddplugin'
 		}],
 	},
-
+*/
 	initComponent: function() {
 		
 		var bar_child = [];
@@ -137,7 +138,14 @@ Ext.define('canopsis.lib.view.ctree' ,{
 		//-------------------------------------------------------
 		this.callParent(arguments);
 		
+		this.ddplugin = Ext.PluginManager.create({ptype : 'treeviewdragdrop'})
+		this.ddplugin.init(this.getView())
 	},
+	
+	destroy: function(){
+		this.ddplugin.destroy()
+		Ext.tree.Panel.superclass.destroy.call(this);
+	}
 	
 	
 });
