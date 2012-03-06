d871489aa673f73df6c9d500a9bfa86c8cb89f1e
Tristan Carranante <tcarranante@capensis.fr>
2012-03-06 12:13:35 +0100

    add mapping to _id/id, better

--- sources/webcore/var/www/canopsis/app/model/Group.js
+++ sources/webcore/var/www/canopsis/app/model/Group.js
@@ -21,7 +21,8 @@
 Ext.define('canopsis.model.Group', {
 	extend: 'Ext.data.Model',
 	fields: [
-		{name : 'id'},
+		{name: '_id'},
+		{name: 'id', mapping:'_id'},
 		{name : 'crecord_type', defaultValue: 'group'},
 		{name : 'crecord_name'},
 	],
