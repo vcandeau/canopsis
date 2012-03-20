519666495e7dc79f0937d0f3b63ce003b9999037
William Pain <wpain@capensis.fr>
2012-03-20 14:00:18 +0100

    Fix parsing and perf_data_array

--- sources/canolibs/lib/canolibs/unittest/ctools-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/ctools-Myunittest.py
@@ -31,38 +31,48 @@ class KnownValues(unittest.TestCase):
 		pass
 
 	def test_01_Perfdata(self):
-		result = {'load1': {'warn': 5.0, 'crit': 10.0, 'metric': 'load1', 'value': 0.44, 'min': 0.0}, 'load15': {'warn': 3.0, 'crit': 4.0, 'metric': 'load15', 'value': 0.13, 'min': 0.0}, 'load5': {'warn': 4.0, 'crit': 6.0, 'metric': 'load5', 'value': 0.19, 'min': 0.0}}
+		result = [{'warn': 5.0, 'crit': 10.0, 'metric': 'load1', 'value': 0.44, 'min': 0.0}, {'warn': 4.0, 'crit': 6.0, 'metric': 'load5', 'value': 0.19, 'min': 0.0}, {'warn': 3.0, 'crit': 4.0, 'metric': 'load15', 'value': 0.13, 'min': 0.0}]
 		perf_data = "load1=0.440;5.000;10.000;0; load5=0.190;4.000;6.000;0; load15=0.130;3.000;4.000;0;"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
 			print perf_data
-			raise Exception('Error in perfdata parsing ...')
+			raise Exception('[1] Error in perfdata parsing ...')
 
-		result = {'load1': {'metric': 'load1', 'value': 0.440}}
+		result = [ {'metric': 'load1', 'value': 0.440} ]
 		perf_data = "load1=0.440"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
 			print perf_data
-			raise Exception('Error in perfdata parsing ...')
+			raise Exception('[2] Error in perfdata parsing ...')
 
 
-		result = {'warn': {'min': 0.0, 'max': 100.0, 'metric': 'warn', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, 'crit': {'min': 0.0, 'max': 100.0, 'metric': 'crit', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, 'ok': {'min': 0.0, 'max': 100.0, 'metric': 'ok', 'value': 100.0, 'warn': 98.0, 'crit': 95.0, 'unit': '%'}}
+		result = [{'min': 0.0, 'max': 100.0, 'metric': 'ok', 'value': 100.0, 'warn': 98.0, 'crit': 95.0, 'unit': '%'}, {'min': 0.0, 'max': 100.0, 'metric': 'warn', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, {'min': 0.0, 'max': 100.0, 'metric': 'crit', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}]
 		perf_data = "'ok'=100.0%;98;95;0;100 'warn'=0%;0;0;0;100 'crit'=0%;0;0;0;100"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
 			print perf_data
-			raise Exception('Error in perfdata parsing ...')
+			raise Exception('[3] Error in perfdata parsing ...')
 
-		result = {'warn: /ing': {'min': 0.0, 'max': 100.0, 'metric': 'warn: /ing', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, 'D:\ Used': {'min': 0.0, 'max': 100.0, 'metric': 'D:\ Used', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, 'C:/ Used': {'min': 0.0, 'max': 100.0, 'metric': 'C:/ Used', 'value': 100.0, 'warn': 98.0, 'crit': 95.0, 'unit': '%'}}
+		result = [{'min': 0.0, 'max': 100.0, 'metric': 'C:/ Used', 'value': 100.0, 'warn': 98.0, 'crit': 95.0, 'unit': '%'}, {'min': 0.0, 'max': 100.0, 'metric': 'warn: /ing', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}, {'min': 0.0, 'max': 100.0, 'metric': 'D:\\ Used', 'value': 0.0, 'warn': 0.0, 'crit': 0.0, 'unit': '%'}]
 		perf_data = "'C:/ Used'=100.0%;98;95;0;100; 'warn: /ing'=0%;0;0;0;100; 'D:\ Used'=0%;0;0;0;100;"
 		perf_data = parse_perfdata(perf_data)
 		if perf_data != result:
 			print perf_data
-			raise Exception('Error in perfdata parsing ...')
+			raise Exception('[4] Error in perfdata parsing ...')
 			
 			
 		perf_data = "/=541MB;906;956;0;1007 /home=62MB;452;477;0;503 /tmp=38MB;906;956;0;1007 /var=943MB;1813;1914;0;2015 /usr=2249MB;5441;5743;0;6046 /opt=68MB;112;118;0;125 /boot=25MB;90;95;0;100 /backup=4410MB;7256;7659;0;8063 /mnt/NAS_OPU_LIVRAISON=35603MB;36300;38317;0;40334 /products/admin=595MB;906;956;0;1007 /products/oracle/10.2.0=146MB;7256;7659;0;8063 /products/agtgrid=851MB;6349;6702;0;7055 /app/PPOPVGL=10966MB;27213;28725;0;30237"
 		perf_data = parse_perfdata(perf_data)
+
+		perf_data = "/=217MB;460;486;0;512 /usr=2757MB;3052;3222;0;3392 /var=397MB;921;972;0;1024 /tmp=437MB;2275;2401;0;2528 /home=243MB;460;486;0;512 /admin=0MB;115;121;0;128 /opt=489MB;691;729;0;768 /var/adm/ras/livedump=4MB;460;486;0;512 /products=2007MB;9216;9728;0;10240 /products/logs=0MB;921;972;0;1024 /products/admin=216MB;921;972;0;1024 /data/PPOPBGL/oracle/PPOPBGL1/nfsbck=32226MB;46771;49369;0;51968 /data/PPOPBGL/oracle/PPOPBGL2/nfsbck=32226MB;46771;49369;0;51968 /app/P1TECGL/P1OPBGLDB01=409MB;921;972;0;1024 /app/admin=0MB;921;972;0;1024 /app/agtgrid=2433MB;2764;2918;0;3072 /data/PPOPBGL/oracle/PPOPBGL1/admin= /data/PPOPBGL/oracle/PPOPBGL1/backup=55MB;1843;1945;0;2048 /data/PPOPBGL/oracle/PPOPBGL1/dbs/archivelog=6605MB;74628;78774;0;82920 /data/PPOPBGL/oracle/PPOPBGL1/dbs/data01=9263MB;94924;100198;0;105472 /data/PPOPBGL/oracle/PPOPBGL1/dbs/flash_recovery_area=0MB;3600;3800;0;4000 /data/PPOPBGL/oracle/PPOPBGL1/dbs/index0= /data/PPOPBGL/oracle/PPOPBGL1/dbs/redolog01="
+		perf_data = parse_perfdata(perf_data)
+		
+		perf_data = "load1=0.040;3.000;4.000;0; load5=0.070;4.000;5.000;0; load15=0.010;4.000;5.000;0;"
+		perf_data = parse_perfdata(perf_data)
+		
+		perf_data = "'C:\ %'=67%;10;5; 'C:\'=6.65G;2;1;0;20; 'E:\ %'=31%;10;5; 'E:\'=10870.0M;1567.45;783.72;0;15674.52; 'J:\ %'=88%;10;5; 'J:\'=2.57G;2;1;0;20.00; 'T:\ %'=100%;10;5; 'T:\'=49.53M;512.3;256.19;0;5123.90;"
+		perf_data = parse_perfdata(perf_data)
+
 
 	def test_02_calcul_pct(self):
 		result = {'unknown': 23.01, 'warning': 41.0, 'ok': 26.55, 'critical': 9.44}
