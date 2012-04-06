6f764eca14c5c9b1d6dd5e099208503183ac8150
Geoffrey Lehée <glehee@capensis.fr>
2012-04-06 17:56:08 +0200

    Modifie python-libs install, .bash_profile and reporting task

--- sources/wkhtmltopdf-libs/etc/tasks.d/task_reporting.py
+++ sources/wkhtmltopdf-libs/etc/tasks.d/task_reporting.py
@@ -5,54 +5,15 @@ from crecord import crecord
 from cstorage import cstorage
 from cfile import cfile
 from datetime import date
+from celerylibs import decorators
 import os, sys, json
 import time
 
 init 	= cinit()
 logger 	= init.getLogger('Reporting Task') 
 
-def stock_result_in_db(func):
-	def render_pdf(*args,**kwargs):
-		
-		try:
-			my_func = func(*args)
-			logger.info('Task successfully done')
-		except Exception, err:
-			function_error = err
-			logger.error(err)
-			
-		#check if the task was scheduled
-		if len(kwargs) != 0:
-			#get account/storage
-			if isinstance(args[4],unicode):
-				account = caccount(user=args[4])
-			else:
-				account = args[4]
-			
-			storage = cstorage(account=account, namespace='task_log')
-			
-			#the function have succeed ?
-			if my_func:
-				if isinstance(my_func, list):
-					data = my_func
-				else:
-					data = [my_func]
-	
-				timestamp = int(time.time())
-				log = {'success': True,'total':1,'output':'Task done','timestamp':timestamp,'data':data}
-			else:
-				log = {'success': False,'total':1,'output':function_error,'timestamp':timestamp}
-
-			log_record = crecord(log,name=kwargs['periodic_task_id'])
-			storage.put(log_record)
-			
-		return my_func
-	return render_pdf
-
-
-
 @task
-@stock_result_in_db
+@decorators.stock_result_in_db
 def render_pdf(filename=None, viewname=None, starttime=None, stoptime=None, account=None, wrapper_conf_file=None):
 
 	if viewname is None:
