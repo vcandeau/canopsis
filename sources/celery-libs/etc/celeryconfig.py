25713b2e2a921d63377e36a5d6915d7c76566692
Geoffrey Lehée <glehee@capensis.fr>
2012-04-11 17:32:08 +0200

    Remove old celery schedule tasks for new aps

--- sources/celery-libs/etc/celeryconfig.py
+++ sources/celery-libs/etc/celeryconfig.py
@@ -3,8 +3,7 @@ from celerylibs import listing
 BROKER_HOST 			= "localhost"
 BROKER_PORT 			= 5672
 BROKER_USER 			= "guest"
-BROKER_PASSWORD			= "guest"
+BROKER_PASSWORD		= "guest"
 BROKER_VHOST 			= "canopsis"
-CELERY_RESULT_BACKEND 	= "amqp"
-CELERY_IMPORTS 			= listing.tasks('/opt/canopsis/etc/tasks.d')
-CELERYBEAT_SCHEDULE		= listing.crons()
+CELERY_RESULT_BACKEND = "amqp"
+CELERY_IMPORTS 				= listing.tasks('/opt/canopsis/etc/tasks.d')
