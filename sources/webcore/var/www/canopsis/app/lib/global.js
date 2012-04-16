b710d3b10f35a9fe7a0c6cc0c66ee7271bb8d5fa
Tristan Carranante <tcarranante@capensis.fr>
2012-04-16 12:00:26 +0200

    load locale.js lib before global.js + add auto translate numberToMonth variable

--- sources/webcore/var/www/canopsis/app/lib/global.js
+++ sources/webcore/var/www/canopsis/app/lib/global.js
@@ -84,5 +84,20 @@ var global = {
 		year : 31557600
 	},
 	
+	numberToMonth:{
+		1: _('January'),
+		2: _('February'),
+		3: _('March'),
+		4: _('April'),
+		5: _('May'),
+		6: _('June'),
+		7: _('July'),
+		8: _('August'),
+		9: _('September'),
+		10: _('October'),
+		11: _('November'),
+		12: _('December'),
+	},
+	
 	untranslated : [],
 }
