1169d059d5024d82e1803a137baf391f714e80ec
Tristan Carranante <tcarranante@capensis.fr>
2012-05-10 15:24:22 +0200

    naming convention

--- sources/webcore/var/www/canopsis/app/model/Widget.js
+++ sources/webcore/var/www/canopsis/app/model/Widget.js
@@ -18,7 +18,7 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 */
-Ext.define('canopsis.model.widget', {
+Ext.define('canopsis.model.Widget', {
     extend: 'Ext.data.Model',
     fields: [
 		{name : 'name'},
--- sources/webcore/var/www/canopsis/app/store/Widget.js
+++ sources/webcore/var/www/canopsis/app/store/Widget.js
@@ -20,7 +20,7 @@
 */
 Ext.define('canopsis.store.Widget', {
     extend: 'canopsis.lib.store.cstore',
-	model: 'canopsis.model.widget',
+	model: 'canopsis.model.Widget',
 	
 	storeId: 'store.Widget',
 	
