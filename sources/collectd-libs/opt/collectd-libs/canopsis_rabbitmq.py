1d319840ecbc606a285b8b7730cd9bb78f56b375
William Pain <wpain@capensis.fr>
2012-05-14 10:58:08 +0200

    Tweaks and add headers

--- sources/collectd-libs/opt/collectd-libs/canopsis_rabbitmq.py
+++ sources/collectd-libs/opt/collectd-libs/canopsis_rabbitmq.py
@@ -1,19 +1,33 @@
+#!/usr/bin/env python
+#--------------------------------
+# Copyright (c) 2011 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
 import collectd
 
 import json
-import urllib2
-
 plugin_name = "canopsis_rabbitmq"
 
 url = "http://127.0.0.1:55672/api"
 
 canopsis_exchanges = ['canopsis.events','canopsis.alerts']
 
-password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-password_mgr.add_password(None, url, "guest", "guest")
-handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-opener = urllib2.build_opener(handler)
-
+opener = None
 
 ### Functions
 def put_value(metric, value, type='gauge'):
@@ -31,8 +45,16 @@ def log(msg):
 ### Callbacks
 def init_callback():
 	log('Init plugin')
-	
 
+	import urllib2
+
+	global opener
+	
+	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+	password_mgr.add_password(None, url, "guest", "guest")
+	handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+	
+	opener = urllib2.build_opener(handler)
 
 def config_callback(config):
 	log('Config plugin')
