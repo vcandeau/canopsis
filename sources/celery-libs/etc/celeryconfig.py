77f5fad1947271488d98c1a26f625148425e2dc7
William Pain <wpain@capensis.fr>
2012-05-15 10:28:09 +0200

    Add CELERYD_TASK_TIME_LIMIT

--- sources/celery-libs/etc/celeryconfig.py
+++ sources/celery-libs/etc/celeryconfig.py
@@ -7,3 +7,5 @@ BROKER_PASSWORD		= "guest"
 BROKER_VHOST 			= "canopsis"
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS 				= listing.tasks('/opt/canopsis/etc/tasks.d')
+
+CELERYD_TASK_TIME_LIMIT = 300
