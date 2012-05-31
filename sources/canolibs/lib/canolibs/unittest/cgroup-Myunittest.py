85e2c2c4f26ce8254073697ac9ee1980009731f6
Tristan Carranante <tcarranante@capensis.fr>
2012-05-31 12:55:21 +0200

    Group function now handle str id account, if storage specified -> get account, add to group and save

--- sources/canolibs/lib/canolibs/unittest/cgroup-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/cgroup-Myunittest.py
@@ -26,6 +26,7 @@ from cstorage import cstorage
 
 STORAGE = None
 ACCOUNT = None
+GROUP = None
 
 class KnownValues(unittest.TestCase): 
 	def setUp(self):
@@ -50,15 +51,16 @@ class KnownValues(unittest.TestCase):
 			
 	def test_04_Store(self):
 		STORAGE.put(GROUP)
+		STORAGE.put(ACCOUNT)
 		GROUP.cat()
 		
 	def test_05_CheckGet(self):
-		record = STORAGE.get("group.group_name")
+		record = STORAGE.get('group.group_name')
 		record.cat()
 		GROUP = cgroup(record)
 		GROUP.cat()
 		
-		if len(GROUP.account_ids) != 1:
+		if ACCOUNT._id not in GROUP.account_ids:
 			raise Exception('group.account_ids: Corruption in load...')
 		
 	def test_06_remove_accounts(self):
@@ -70,8 +72,37 @@ class KnownValues(unittest.TestCase):
 			raise Exception('Error while add_accounts, group not added to account')
 	
 	def test_07_Remove(self):
+		STORAGE.remove(GROUP)
 		STORAGE.remove(ACCOUNT)
 		
+	def test_08_cgroup_with_storage(self):
+		global GROUP
+		GROUP = cgroup(name='group_name', storage=STORAGE)
+		STORAGE.put(GROUP)
+		
+		global ACCOUNT
+		ACCOUNT = caccount(user="wpain", lastname="Pain", firstname="William", mail="wpain@capensis.fr", group="capensis")
+		STORAGE.put(ACCOUNT)
+		
+	def test_09_add_from_str_ids(self):
+		GROUP.add_accounts(ACCOUNT._id)
+		if ACCOUNT._id not in GROUP.account_ids:
+			raise Exception('Error while add_accounts, account not added')
+			
+		bdd_account = caccount(STORAGE.get(ACCOUNT._id))
+		if GROUP._id not in bdd_account.groups:
+			raise Exception('Error while add_accounts, group not added to account')
+	
+		
+	def test_10_remove_from_str_ids(self):
+		GROUP.remove_accounts(ACCOUNT._id)
+		if ACCOUNT._id in GROUP.account_ids:
+			raise Exception('Error while add_accounts, account not added')
+			
+		bdd_account = caccount(STORAGE.get(ACCOUNT._id))
+		if GROUP._id in bdd_account.groups:
+			raise Exception('Error while add_accounts, group not added to account')
+
 	def test_99_DropNamespace(self):
 		STORAGE.drop_namespace('unittest')
 		
