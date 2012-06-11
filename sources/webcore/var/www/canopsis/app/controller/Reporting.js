b2774ac5311e4b821564e3a28f1ae39087f8ab34
Tristan Carranante <tcarranante@capensis.fr>
2012-06-11 17:46:00 +0200

    schedule option "run now" take the right time interval

--- sources/webcore/var/www/canopsis/app/controller/Reporting.js
+++ sources/webcore/var/www/canopsis/app/controller/Reporting.js
@@ -36,7 +36,7 @@ Ext.define('canopsis.controller.Reporting', {
 		//if no date given
 		if(to == undefined){
 			to = Date.now() 
-			from = to - (global.commonTs.day*1000)
+			from = to - (from*1000)
 		}
 		
 		global.notify.notify(_('Please Wait'),_('Your document is rendering, a popup will ask you where to save in few seconds'))
