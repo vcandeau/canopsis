486e802d2a779316abe6f8365f64ff89f32ef1c4
William Pain <wpain@capensis.fr>
2012-06-15 10:44:35 +0200

    Small fixes

--- sources/webcore/var/www/canopsis/widgets/stream/stream.js
+++ sources/webcore/var/www/canopsis/widgets/stream/stream.js
@@ -110,15 +110,17 @@ Ext.define('widgets.stream.stream' ,{
 		var me = this
 		now.stream_getHistory(this.max, function(records){
 			log.debug("Load "+records.length+" events", me.logAuthor)
-				if (records.length > 0)
-					for (var i in records)
-							records[i] = Ext.create('widgets.stream.event', {id: me.get_event_id(records[i]), raw: records[i], stream: me});
-					me.add_events(records);
-					
-				if (! me.reportMode )
-					me.subscribe();
-					
-				me.ready();
+			if (records.length > 0){
+				for (var i in records)
+						records[i] = Ext.create('widgets.stream.event', {id: me.get_event_id(records[i]), raw: records[i], stream: me});
+				
+				me.add_events(records);
+			}
+			
+			if (! me.reportMode )
+				me.subscribe();
+				
+			me.ready();
 		});
 		
 	},
