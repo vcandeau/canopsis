17b802469ed8c99c671452c84b6396db25e0381b
William Pain <wpain@capensis.fr>
2012-06-15 11:45:52 +0200

    Check now before use

--- sources/webcore/var/www/canopsis/widgets/stream/stream.js
+++ sources/webcore/var/www/canopsis/widgets/stream/stream.js
@@ -108,20 +108,24 @@ Ext.define('widgets.stream.stream' ,{
 		
 		// Load history
 		var me = this
-		now.stream_getHistory(this.max, function(records){
-			log.debug("Load "+records.length+" events", me.logAuthor)
-			if (records.length > 0){
-				for (var i in records)
-						records[i] = Ext.create('widgets.stream.event', {id: me.get_event_id(records[i]), raw: records[i], stream: me});
+		if (now){
+			now.stream_getHistory(this.max, function(records){
+				log.debug("Load "+records.length+" events", me.logAuthor)
+				if (records.length > 0){
+					for (var i in records)
+							records[i] = Ext.create('widgets.stream.event', {id: me.get_event_id(records[i]), raw: records[i], stream: me});
+					
+					me.add_events(records);
+				}
 				
-				me.add_events(records);
-			}
-			
-			if (! me.reportMode )
-				me.subscribe();
-				
-			me.ready();
-		});
+				if (! me.reportMode )
+					me.subscribe();
+					
+				me.ready();
+			});
+		}else{
+			log.error("'now' is undefined, websocket down ?", me.logAuthor)
+		}
 		
 	},
 	
