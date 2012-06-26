e52726cb3b94230ad3f3a05978cfd8f523400dbc
William Pain <wpain@capensis.fr>
2012-06-26 17:38:40 +0200

    Fix scrollbar

--- sources/webcore/var/www/canopsis/widgets/stream/stream.js
+++ sources/webcore/var/www/canopsis/widgets/stream/stream.js
@@ -38,8 +38,7 @@ Ext.define('widgets.stream.stream' , {
 	burst_interval: 500, //ms
 	burst_threshold: 2, //nb events
 
-	autoScroll: true,
-
+	wcontainer_autoScroll: true,
 	wcontainer_layout: 'anchor',
 
 	showToolbar: true,
