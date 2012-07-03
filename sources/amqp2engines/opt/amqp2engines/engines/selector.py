e493b94a77043e56a098dbc259419907b8164b32
William Pain <wpain@capensis.fr>
2012-07-03 15:35:14 +0200

    add: selector engine

--- sources/amqp2engines/opt/amqp2engines/engines/selector.py
+++ sources/amqp2engines/opt/amqp2engines/engines/selector.py
@@ -19,12 +19,122 @@
 # ---------------------------------
 
 from cengine import cengine
-
+from cstorage import get_storage
+from caccount import caccount
+from cselector import cselector
+		
 NAME="selector"
 
 class engine(cengine):
 	def __init__(self, *args, **kargs):
 		cengine.__init__(self, name=NAME, *args, **kargs)
+		self.selectors = {}
 		
-	def work(self, event, msg):		
+		self.nb_beat_interval = 900
+		self.nb_beat = 0
+	
+	def pre_run(self):
+		#load selectors
+		self.storage = get_storage(namespace='object', account=caccount(user="root", group="root"))
+		self.load_selectors()
+		
+	def unload_all_selectors(self):
+		self.logger.debug("Unload all selectors")
+		records = self.storage.find({'crecord_type': 'selector'})
+		for record in records:
+			record.data["loaded"] = False
+			
+		# Save
+		self.storage.put(records, namespace="object")
+	
+	def unload_selectors(self):
+		## Unload selectors
+		if self.selectors:
+			for _id in self.selectors:
+				selector = self.selectors[_id]
+				record = self.storage.get(selector._id)
+				self.logger.debug("Unload selector %s: %s" % (record._id, record.name))
+				record.data["loaded"] = False
+				self.storage.put(record)
+				del selector
+				
+		self.selectors = []
+	
+	def load_selectors(self):
+		## Load selectors
+		
+		## New selector or modified selector
+		records = self.storage.find({'$and': [{'crecord_type': 'selector'}, {'loaded': False}]})
+		
+		for record in records:
+			self.logger.debug("Load selector %s: %s" % (record._id, record.name))
+			_id = record._id
+			try:
+				selector = self.selectors[_id]
+				## Delete old
+				del self.selectors[_id]
+			except:
+				pass
+				
+			## store
+			self.selectors[_id] = cselector(storage=self.storage, record=record)
+		
+			## Publish state	
+			(rk, event) = self.selectors[_id].event()
+			if event:
+				self.amqp.publish(event, rk, self.amqp.exchange_name_events)
+			
+			# Set loaded
+			record.data["loaded"] = True
+			
+		# Save
+		self.storage.put(records, namespace="object")
+
+	def beat(self):
+		self.nb_beat +=1
+		
+		## Send event all self.beat_interval seconds
+		if self.nb_beat >= (self.nb_beat_interval/self.beat_interval):
+			self.nb_beat = 0
+			for _id in self.selectors:
+				selector = self.selectors[_id]
+				(rk, event) = selector.event()
+				if event:
+					self.amqp.publish(event, rk, self.amqp.exchange_name_events)
+				else:
+					pass
+					
+		self.load_selectors()
+		
+	def get_selectors(self, _id):
+		if not self.selectors:
+			return []
+			
+		selectors = []
+		for sid in self.selectors:
+			selector = self.selectors[sid]
+			if selector.match(_id):
+				selectors.append(selector)
+			
+		return selectors
+		
+	def work(self, event, msg):
+		event_id = event["event_id"]
+						
+		## Process selector
+		selectors = self.get_selectors(event_id)
+		if not selectors:
+			return event
+			
+		for selector in selectors:
+			(rk, sevent) = selector.event()
+			if sevent:
+				self.logger.debug("%s: %s" % (rk, sevent))
+				self.amqp.publish(sevent, rk, self.amqp.exchange_name_events)
+			else:
+				pass
+		
 		return event
+		
+	def post_run(self):
+		self.unload_selectors()
