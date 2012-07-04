1e2dccbd588e846442bf3849a76096d443152869
Tristan Carranante <tcarranante@capensis.fr>
2012-07-04 09:13:01 +0200

    fix: json load error

--- sources/webcore/var/www/canopsis/app/lib/form/field/cfilter.js
+++ sources/webcore/var/www/canopsis/app/lib/form/field/cfilter.js
@@ -666,11 +666,15 @@ Ext.define('canopsis.lib.form.field.cfilter' , {
 	},
 
 	setValue: function(value) {
-		if (typeof(value) == 'string')
-			value = Ext.decode(value);
+		log.debug('Set value',this.logAuthor)
+		log.dump(value)
+		if(value != null && value != undefined && value != ''){
+			if (typeof(value) == 'string')
+				value = Ext.decode(value);
 
-		log.debug('The filter to set is : ' + Ext.encode(value), this.logAuthor);
-		this.cfilter.setValue(value);
+			log.debug('The filter to set is : ' + Ext.encode(value), this.logAuthor);
+			this.cfilter.setValue(value);
+		}
 	}
 
 });
