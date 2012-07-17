912e775fd657d3713d48b981be9b0e79eda4cb35
Tristan Carranante <tcarranante@capensis.fr>
2012-07-17 11:51:54 +0200

    add: fixed css + comment zone improved

--- sources/webcore/var/www/canopsis/widgets/weather/weather.js
+++ sources/webcore/var/www/canopsis/widgets/weather/weather.js
@@ -18,27 +18,27 @@
 # along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------
 */
-widget_weather_template = new Ext.Template(
+widget_weather_template = Ext.create('Ext.XTemplate',
 		'<table class="table">',
 			'<tr>',
 				'<td class="left_panel">',
-					'<p class="title">title</p>',
-					'<p class="comment">comment</p>',
+					'<p class="title">{title}</p>',
+					'<p class="comment">{output}</p>',
 					'<div class="alert_panel">',
-						'<div>button</div>',
-						'<div><img src=""/></div>',
-						'<div><span>alert comment</span></div>',
+						'<tpl if="button_text != undefined">',
+							'<div class="alert_button"><button type="button">{button_text}</button></div>',
+						'</tpl>',
+						'<div class="alert_img"></div>',
+						'<div class="alert_information"><span>{alert_comment}</span></div>',
 					'</div>',
 				'</td>',
 				'<td class="right_panel">',
-					'<div class="logo">logo</div>',
-					'<div class="legend">text</div>',
+					'<div class="logo {class_icon}"><p>{percent}%</p></div>',
+					'<div class="legend">{legend}</div>',
 				'</td>',
 			'</tr>',
 		'</table',
-		{
-			compiled: true,      // compile immediately
-		}
+		{compiled: true}
 	);
 
 Ext.define('widgets.weather.weather' , {
@@ -46,29 +46,86 @@ Ext.define('widgets.weather.weather' , {
 
 	alias: 'widget.weather',
 	logAuthor: '[widget][weather]',
-	
-	_template : undefined,
+	border: false,
 	
 	cls: 'widget-weather',
 	
+	iconSet : '01',
+	
+	option_button : true,
+	
 	initComponent: function() {
 		log.debug('Initialize...' , this.logAuthor)
-		/*
-		this.inner_component = Ext.create('Ext.Component',{
-			html : this.build()
-		})*/
-
 		this.callParent(arguments);
-
 	},
 	
-	afterContainerRender: function() {
-		//this.wcontainer.removeAll()
-		this.wcontainer.update(this.build())
+	build : function(data){
+		var _html = widget_weather_template.applyTemplate(data);
+		this.wcontainer.update(_html)
+		return _html
 	},
 	
-	build : function(){
-		return widget_weather_template.applyTemplate();
+	onRefresh : function(data){
+		if(data.event_type == 'sla'){	
+			//build data
+			var widget_data = {}
+			
+			widget_data.title = data.component
+			widget_data.output = data.output
+			widget_data.legend = rdr_tstodate(data.timestamp)
+			widget_data.percent = data.perf_data_array[0].value
+			widget_data.class_icon = this.getIcon(widget_data.percent)
+			
+			if(this.option_button == true)
+				widget_data.button_text = _('Report an issue')
+				
+			widget_data.alert_comment = 'This component will be shut down from 0:00am to 9:00am'
+			
+			this.build(widget_data)
+			log.dump(this.border)
+		} else {
+			this.wcontainer.update('invalid selector')
+		}
+	},
+	
+	getIcon: function(value){
+		value = Math.floor(value/10) *10
+		switch(value){
+			case 0:
+				return 'iconSet' + this.iconSet + '_' + '0-10'
+				break;
+			case 10:
+				return 'iconSet' + this.iconSet + '_' + '10-20'
+				break;
+			case 20:
+				return 'iconSet' + this.iconSet + '_' + '20-30'
+				break;
+			case 30:
+				return 'iconSet' + this.iconSet + '_' + '30-40'
+				break;
+			case 40:
+				return 'iconSet' + this.iconSet + '_' + '40-50'
+				break;
+			case 50:
+				return 'iconSet' + this.iconSet + '_' + '50-60'
+				break;
+			case 60:
+				return 'iconSet' + this.iconSet + '_' + '60-70'
+				break;
+			case 70:
+				return 'iconSet' + this.iconSet + '_' + '70-80'
+				break;
+			case 80:
+				return 'iconSet' + this.iconSet + '_' + '80-90'
+				break;
+			case 90:
+				return 'iconSet' + this.iconSet + '_' + '90-100'
+				break;
+			case 100:
+				return 'iconSet' + this.iconSet + '_' + '90-100'
+				break;
+		}
 	}
+	
 	
 });
