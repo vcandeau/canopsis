790a584536cff9fb34e50ff19b9691084ac214ac
William Pain <wpain@capensis.fr>
2012-07-20 12:42:04 +0200

    add: options

--- sources/webcore/var/www/canopsis/widgets/stream/stream.js
+++ sources/webcore/var/www/canopsis/widgets/stream/stream.js
@@ -29,6 +29,9 @@ Ext.define('widgets.stream.stream' , {
 
 	max: 10,
 	max_comment: 5,
+	
+	enable_userinputs: true,
+	enable_comments: true,
 
 	tags: "",
 
@@ -55,72 +58,84 @@ Ext.define('widgets.stream.stream' , {
 			this.tags = this.tags.split(' ');
 		}
 
+		if (! this.showToolbar)
+			this.enable_userinputs = false
+
 		if (this.showToolbar && ! this.exportMode) {
 			
-			this.tbar = Ext.create('Ext.toolbar.Toolbar', {
-					//baseCls: 'x-panel-header',
-					//height: 27,
-					items: [
-						{
-							xtype: 'tbtext',
-							text: "<img src='widgets/stream/logo/ui.png' height='19' width='19'></img>"
-						},{
-							xtype: 'combobox',
-							id: this.id + '-state',
-							queryMode: 'local',
-							displayField: 'text',
-							valueField: 'value',
-							width: 70,
-							value: 0,
-							store: {
-								xtype: 'store',
-								fields: ['value', 'text'],
-								data: [
-									{value: 0, text: 'Ok'},
-									{value: 1, text: 'Warning'},
-									{value: 2, text: 'Critical'}
-								]
-							}
-						},{
-							xtype: 'textfield',
-							emptyText: _('Leave a') + ' ' + _('event') + ' ?',
-							id: this.id + '-message',
-							width: 300,
-							listeners: {
-								specialkey: {
-									fn: function(field, e) {
-										if (e.getKey() == e.ENTER)
-											this.publish_event();
-									},
-									scope: this
-								}
-
-							}
-						},
-						'->', {
-							iconCls: 'icon-control-repeat',
-							tooltip: _('Clear tray'),
-							scope: this,
-							handler: function() {
-								this.wcontainer.removeAll(true);
-							}
-						},{
-							iconCls: 'icon-control-pause',
-							tooltip: _('Pause stream'),
-							scope: this,
-							enableToggle: true,
-							toggleHandler: function(button, state) {
-								if (state) {
-									button.setIconCls('icon-control-play');
-									this.unsubscribe();
-								}else {
-									button.setIconCls('icon-control-pause');
-									this.subscribe();
-								}
+			var items = []
+			
+			if (this.enable_userinputs){
+				items = items.concat([
+					{
+						xtype: 'tbtext',
+						text: "<img src='widgets/stream/logo/ui.png' height='19' width='19'></img>"
+					},{
+						xtype: 'combobox',
+						id: this.id + '-state',
+						queryMode: 'local',
+						displayField: 'text',
+						valueField: 'value',
+						width: 70,
+						value: 0,
+						store: {
+							xtype: 'store',
+							fields: ['value', 'text'],
+							data: [
+								{value: 0, text: 'Ok'},
+								{value: 1, text: 'Warning'},
+								{value: 2, text: 'Critical'}
+							]
+						}
+					},{
+						xtype: 'textfield',
+						emptyText: _('Leave a') + ' ' + _('event') + ' ?',
+						id: this.id + '-message',
+						width: 300,
+						listeners: {
+							specialkey: {
+								fn: function(field, e) {
+									if (e.getKey() == e.ENTER)
+										this.publish_event();
+								},
+								scope: this
 							}
 						}
-					]
-				});
+					}			
+				]);
+			}
+			
+			
+			items = items.concat([
+				'->', {
+						iconCls: 'icon-control-repeat',
+						tooltip: _('Clear tray'),
+						scope: this,
+						handler: function() {
+							this.wcontainer.removeAll(true);
+						}
+					},{
+						iconCls: 'icon-control-pause',
+						tooltip: _('Pause stream'),
+						scope: this,
+						enableToggle: true,
+						toggleHandler: function(button, state) {
+							if (state) {
+								button.setIconCls('icon-control-play');
+								this.unsubscribe();
+							}else {
+								button.setIconCls('icon-control-pause');
+								this.subscribe();
+							}
+						}
+					}			
+			]);
+			
+			this.tbar = Ext.create('Ext.toolbar.Toolbar', {
+				//baseCls: 'x-panel-header',
+				//height: 27,
+				items: items
+			});
 		}
 
 		this.callParent(arguments);
