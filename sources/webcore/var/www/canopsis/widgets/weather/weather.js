e638bbb67573885f0cb939783fb92c1804c81c65
Tristan Carranante <tcarranante@capensis.fr>
2012-07-20 10:17:48 +0200

    add: notification when sla not found

--- sources/webcore/var/www/canopsis/widgets/weather/weather.js
+++ sources/webcore/var/www/canopsis/widgets/weather/weather.js
@@ -66,7 +66,8 @@ Ext.define('widgets.weather.weather' , {
 					this.populate(data);
 				},
 				failure: function(result, request) {
-					log.error('Impossible to get Node informations, Ajax request failed ... ('+ request.url + ')', this.logAuthor);
+					log.error('Impossible to get', this.logAuthor);
+					global.notify.notify(_('No SLA available'), _('Currently there is no SLA for this selector, please try later'),'info');
 				}
 			});
 		}
