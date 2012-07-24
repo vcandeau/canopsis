2ad0f413f74a92313984d572bc3915cd4b254db2
William Pain <wpain@capensis.fr>
2012-07-24 10:30:26 +0200

    add: search on tags with prefix '#'

--- sources/webcore/var/www/canopsis/app/lib/controller/cgrid.js
+++ sources/webcore/var/www/canopsis/app/lib/controller/cgrid.js
@@ -675,20 +675,35 @@ Ext.define('canopsis.lib.controller.cgrid', {
 			log.debug(' + Search:', this.logAuthor);
 			log.dump(search_value_array);
 			
+			// for each opt_bar_search_field
 			for (var i in grid.opt_bar_search_field) {
 				var field = grid.opt_bar_search_field[i]
 				
 				if (search_value_array.length == 1){
 					var filter = {};
-					filter[field] = { '$regex' : search, "$options": 'i'};
+					
+					// Process tags
+					if (search[0] == '#')
+						filter = {'tags': search.slice(1)};
+					else
+						filter[field] = { '$regex' : search, "$options": 'i'};
 				
 					search_filters.push(filter);
 				
 				} else {
 					var filter = []
+					
+					// for each word in search string
 					for (var j in search_value_array){
 						var sub_filter = {}
-						sub_filter[field] = { '$regex' : search_value_array[j], "$options": 'i'}
+						var word = search_value_array[j]
+						
+						// Process tags
+						if (word[0] == '#')
+							sub_filter = {'tags': word.slice(1)};
+						else
+							sub_filter[field] = { '$regex' : word, "$options": 'i'}
+							
 						filter.push(sub_filter)
 					}
 					search_filters.push({ "$and": filter})
