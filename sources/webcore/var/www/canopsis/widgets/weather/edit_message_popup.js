1f39a26072b616fc54e4fed4b1202209af46326a
Tristan Carranante <tcarranante@capensis.fr>
2012-07-31 11:19:40 +0200

    add: messagebox for changing selector output

---
+++ sources/webcore/var/www/canopsis/widgets/weather/edit_message_popup.js
@@ -0,0 +1,44 @@
+/*
+#--------------------------------
+# Copyright (c) 2011 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+*/
+
+Ext.define('widgets.weather.edit_message_popup' , {
+	extend: 'canopsis.lib.view.cpopup',
+	alias: 'widget.weather.edit_message_popup',
+	
+	textAreaLabel : 'Type the new displayed message ',
+	
+	initComponent: function() {
+		this.textAreaLabel += 'for ' + this.component
+		this.callParent(arguments);
+	},
+	
+	ok_button_function : function(){
+		log.debug('clicked on ok button',this.logAuthor)
+		global.selectorCtrl.change_selector_output(
+			this.referer,
+			this.event_type,
+			this.input_textArea.getValue()
+		)
+		this.close()
+	}
+	
+	
+})
